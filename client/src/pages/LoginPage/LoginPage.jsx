5e936a205ccc497e1c9cc83444db165ea6ba9040
hambergjesse <hambergjesse@gmail.com>
2023-02-10 14:42:04 +0200

    comment removal

--- client/src/pages/LoginPage/LoginPage.jsx
+++ client/src/pages/LoginPage/LoginPage.jsx
@@ -14,102 +14,99 @@ import * as EmailValidator from "email-validator";
 import { passwordVal } from "../../utils/PasswordValidate";
 
 const LoginPage = () => {
-  const [email, setEmail] = useState(""),
-    [password, setPassword] = useState(""),
-    [buttonDisabled, setButtonDisabled] = useState();
+	const [email, setEmail] = useState(""),
+		[password, setPassword] = useState(""),
+		[buttonDisabled, setButtonDisabled] = useState();
 
-  // stores form input info
-  const formRef = useRef(),
-    emailRef = useRef(),
-    passwordRef = useRef();
+	// stores form input info
+	const formRef = useRef(),
+		emailRef = useRef(),
+		passwordRef = useRef();
 
-  // change page
-  // const navigate = useNavigate();
+	// processes the login after fields have been filled and the "login" button has been pressed
+	const processLogin = () => {
+		const email = emailRef.current.value;
+		const password = passwordRef.current.value;
 
-  // processes the login after fields have been filled and the "login" button has been pressed
-  const processLogin = () => {
-    const email = emailRef.current.value;
-    const password = passwordRef.current.value;
+		// validate inputs
+		console.log(EmailValidator.validate(email));
+		console.log(passwordVal.validate(password));
 
-    // validate inputs
-    console.log(EmailValidator.validate(email));
-    console.log(passwordVal.validate(password));
+		// sending data from form to the backend
+		axios
+			.post("/login", {
+				email: email,
+				password: password,
+			})
+			.then(function (res) {
+				console.log(res);
+			})
+			.catch(function (err) {
+				console.log(err);
+			});
+	};
 
-    // sending data from form to the backend
-    axios
-      .post("/login", {
-        email: email,
-        password: password,
-      })
-      .then(function (res) {
-        console.log(res);
-      })
-      .catch(function (err) {
-        console.log(err);
-      });
-  };
+	// enable login button style if fields are filled
+	useEffect(() => {
+		setButtonDisabled(![email, password].every((input) => input.length > 0));
+	}, [email, password]);
 
-  // enable login button style if fields are filled
-  useEffect(() => {
-    setButtonDisabled(![email, password].every((input) => input.length > 0));
-  }, [email, password]);
+	// button styling/CSS
+	const buttonStyleDisabled = {
+			color: "var(--saukko-main-white)",
+			border: "var(--link-disabled)",
+			background: "var(--link-disabled)",
+		},
+		buttonStyleEnabled = {
+			color: "var(--saukko-main-white)",
+			border: "var(--saukko-main-black)",
+			background: "var(--saukko-main-black)",
+		};
 
-  // button styling/CSS
-  const buttonStyleDisabled = {
-      color: "var(--saukko-main-white)",
-      border: "var(--link-disabled)",
-      background: "var(--link-disabled)",
-    },
-    buttonStyleEnabled = {
-      color: "var(--saukko-main-white)",
-      border: "var(--saukko-main-black)",
-      background: "var(--saukko-main-black)",
-    };
-
-  return (
-    <main className="loginPage__wrapper">
-      <WavesHeader title="Saukko" fill="#9fc9eb" />
-      <section className="loginPage__container">
-        <h2>Kirjaudu sisään</h2>
-        <form ref={formRef} onSubmit={processLogin}>
-          <section className="loginPage__container--form-text">
-            <label htmlFor="">Sähköposti *</label>
-            <input
-              ref={emailRef}
-              type="email"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              placeholder="Kirjoita sähköpostiosoitteesi."
-            />
-            <label htmlFor="">Salasana *</label>
-            <input
-              ref={passwordRef}
-              type="password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              placeholder="Kirjoita salasanasi."
-            />
-            <a href="/forgot-password">Unohtuiko salasana?</a>
-          </section>
-        </form>
-      </section>
-      <section className="loginPage__form--bottom">
-        <p>
-          Eikö ole vielä tiliä? <a href="/register">Luo tili</a>
-        </p>
-        <Button
-          style={buttonDisabled ? buttonStyleDisabled : buttonStyleEnabled}
-          onClick={() =>
-            buttonDisabled ? console.log("button disabled") : processLogin()
-          }
-          type="submit"
-          text="Kirjaudu sisään"
-        />
-      </section>
-    </main>
-  );
+	return (
+		<main className="loginPage__wrapper">
+			<WavesHeader title="Saukko" fill="#9fc9eb" />
+			<section className="loginPage__container">
+				<h2>Kirjaudu sisään</h2>
+				<form ref={formRef} onSubmit={processLogin}>
+					<section className="loginPage__container--form-text">
+						<label htmlFor="">Sähköposti *</label>
+						<input
+							ref={emailRef}
+							type="email"
+							onChange={(e) => {
+								setEmail(e.target.value);
+							}}
+							placeholder="Kirjoita sähköpostiosoitteesi."
+						/>
+						<label htmlFor="">Salasana *</label>
+						<input
+							ref={passwordRef}
+							type="password"
+							onChange={(e) => {
+								setPassword(e.target.value);
+							}}
+							placeholder="Kirjoita salasanasi."
+						/>
+						<a href="/forgot-password">Unohtuiko salasana?</a>
+					</section>
+				</form>
+			</section>
+			<section className="loginPage__form--bottom">
+				<p>
+					Eikö ole vielä tiliä? <a href="/register">Luo tili</a>
+				</p>
+				<Button
+					style={buttonDisabled ? buttonStyleDisabled : buttonStyleEnabled}
+					onClick={() =>
+						buttonDisabled ? console.log("button disabled") : processLogin()
+					}
+					type="submit"
+					text="Kirjaudu sisään"
+				/>
+			</section>
+		</main>
+	);
 };
 
 export default LoginPage;
