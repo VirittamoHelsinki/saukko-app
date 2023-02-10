453bf878ef0bbbfe32181680e3be64f271e787c2
hambergjesse <hambergjesse@gmail.com>
2023-02-10 14:57:09 +0200

    comment removal

--- client/src/components/Router/Router.jsx
+++ client/src/components/Router/Router.jsx
@@ -13,28 +13,27 @@ import FirstLogin from "../../pages/FirstLogin/FirstLogin";
 import SearchPage from "../../pages/SearchPage/SearchPage";
 
 const Router = () => {
-  let location = useLocation();
-  let path = location.pathname;
+    let location = useLocation();
+    let path = location.pathname;
 
-  // browser window scroll-to-top on path change
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [path]);
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [path]);
 
-  return (
-    <>
-      <Routes key={path} location={location}>
-        {/* placeholder paths and pages */}
-        <Route exact="true" path="/" element={<LandingPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/home" element={<UserPage />} />
-        <Route path="/first-login" element={<FirstLogin />} />
-        <Route path="/search" element={<SearchPage />} />
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Routes key={path} location={location}>
+                {/* placeholder paths and pages */}
+                <Route exact="true" path="/" element={<LandingPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/home" element={<UserPage />} />
+                <Route path="/first-login" element={<FirstLogin />} />
+                <Route path="/search" element={<SearchPage />} />
+            </Routes>
+        </>
+    );
 };
 
 export default Router;
