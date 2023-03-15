f76e89a05d0f88849ceb7024e73a11b066263734
hambergjesse <hambergjesse@gmail.com>
2023-03-15 10:30:50 +0200

    Router.jsx fix

--- client/src/components/Router/Router.jsx
+++ client/src/components/Router/Router.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 
 // importing necessary packages for routing
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
 import React, { useContext } from "react";
 
 // importing all pages which need routing
@@ -15,33 +15,57 @@ import SearchPage from "../../pages/SearchPage/SearchPage";
 import AuthContext from "../../utils/context/AuthContext";
 
 const Router = () => {
-  let location = useLocation();
-  const { loggedIn } = useContext(AuthContext);
-  
-  useEffect(() => {
+    let location = useLocation();
+    const { loggedIn } = useContext(AuthContext);
+
+    const path = location.path;
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (
+            loggedIn &&
+            (path === "/" ||
+                path === "/login" ||
+                path === "/register" ||
+                path === "/forgot-password")
+        ) {
+            navigate("/home");
+        }
+    }, [loggedIn, path, navigate]);
+
+    useEffect(() => {
         window.scrollTo(0, 0);
     }, [path]);
-    
-  return (
-    <>
-      <Routes key={location.pathname} location={location}>
-        {/* placeholder paths and pages */}
-        <Route exact="true" path="/" element={<LandingPage />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/background-form" element={<BackgroundForm />} />
-        <Route path="/first-login" element={<FirstLogin />} />
-        <Route path="/search" element={<SearchPage />} /
 
-        {!loggedIn && (
-          <>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-          </>
-        )}
-        {loggedIn && <Route path="/home" element={<UserPage />} />}
-      </Routes>
-    </>
-  );
+    return (
+        <>
+            <Routes key={location.pathname} location={location}>
+                {/* placeholder paths and pages */}
+                <Route path="/forgot-password" element={<ForgotPassword />} />
+                <Route path="/first-login" element={<FirstLogin />} />
+                <Route path="/search" element={<SearchPage />} />
+
+                {!loggedIn && (
+                    <>
+                        <Route
+                            exact="true"
+                            path="/"
+                            element={<LandingPage />}
+                        />
+                        <Route path="/login" element={<LoginPage />} />
+                        <Route path="/register" element={<RegisterPage />} />
+                    </>
+                )}
+                {loggedIn && (
+                    <>
+                        <Route path="/home" element={<UserPage />} />
+                        <Route path="/first-login" element={<FirstLogin />} />
+                        <Route path="/search" element={<SearchPage />} />
+                    </>
+                )}
+            </Routes>
+        </>
+    );
 };
 
 export default Router;
