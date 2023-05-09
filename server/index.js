37726c6025ff2c603b3ae5f1cc8a23f46f66fded
zabisadiqui <zabihullah.sadiqui@gmail.com>
2023-05-09 14:43:25 +0300

    Fixed some minor bugs

--- server/index.js
+++ server/index.js
@@ -11,29 +11,29 @@ const jwt = require("jsonwebtoken");
 app.use(express.json());
 app.use(cookieParser());
 app.use(
-  cors({
-    origin: ["http://localhost:3000"],
-    credentials: true,
-  })
+    cors({
+        origin: ["http://localhost:3000"],
+        credentials: true,
+    })
 );
 
 // connecting database
 mongoose.set("strictQuery", true);
 mongoose
-  .connect(process.env.MDB_CONNECT, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("Database connected!");
-  })
-  .catch((error) => {
-    console.log("Unable to connect database");
-  });
+    .connect(process.env.MDB_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("Database connected!");
+    })
+    .catch((error) => {
+        console.log("Unable to connect database");
+    });
 
 app.use("/auth", require("./routers/userRouter"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Servers running on port ${PORT}`);
+    console.log(`Servers running on port ${PORT}`);
 });
