69d3819320c5712a598daf07d65c641ead3db56b
hnnav <henna.vahatiitto@gmail.com>
2023-05-25 15:25:09 +0300

    styling

--- client/src/pages/ProfilePage/ProfilePage.jsx
+++ client/src/pages/ProfilePage/ProfilePage.jsx
@@ -1,34 +1,57 @@
+// Import react packages
 import React from 'react'
 
+// Import components
 import WavesHeader from "../../components/Header/WavesHeader";
 import Button from "../../components/Button/Button";
+import UserNav from "../../components/UserNav/UserNav";
 
 function ProfilePage() {
-  return (
-    <main className="profile__wrapper">
-        <WavesHeader 
-            title="Saukko"
-            secondTitle="Profiili"
-            disabled={true} 
-        />
 
-        <section className="profile__container">
-            <div className="profile__container--name">
-                <p>Nimi</p>
-            </div>
-            <div className="profile__container--email">
-                <p>Sähköposti</p>
-            </div>
-            <div className="profile__container--password">
-                <p>Vaihda salasana</p>
-            </div>
-        </section>
-        <section className="profile__logout">
-            <Button />
-        </section>
-    </main>
+    // Button styling / CSS
+    const buttonStyle = {
+        color: "var(--link-blue)",
+        background: "var(--saukko-main-white)",
+        paddingLeft: "40px",
+        border: "2px solid var(--link-blue)"
+    };
+    
+    return (
+      <main className="profile__wrapper">
+          <WavesHeader 
+              title="Saukko"
+              secondTitle="Profiili"
+              disabled={true} 
+          />
 
-  )
+          <section className="profile__container">
+              <div>
+                  <p className="profile__container--label">Nimi</p>
+                  <p className="profile__container--value">User Example</p>
+              </div>
+              <div>
+                  <p className="profile__container--label">Sähköposti</p>
+                  <p className="profile__container--value">example@email.com</p>
+              </div>
+              <div>
+                  <p className="profile__container--label">Vaihda salasana</p>
+              </div>
+          </section>
+          <section className="profile__logout">
+                <Button
+                    text="Kirjaudu Ulos"
+                    style={buttonStyle}
+                    /* onClick={() => {
+                        navigate("/register-customer");
+                    }} */
+
+                    icon={"grommet-icons:logout"}
+                />
+          </section>
+          <UserNav />
+      </main>
+
+    )
 }
 
 export default ProfilePage
