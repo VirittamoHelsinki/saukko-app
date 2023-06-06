60ebc76a58c247262db09c18aba4175e4cf45b81
hnnav <henna.vahatiitto@gmail.com>
2023-06-06 11:55:25 +0300

    profile fixed

--- client/src/pages/ChooseDegree/DegreeInfo/DegreeInfo.jsx
+++ client/src/pages/ChooseDegree/DegreeInfo/DegreeInfo.jsx
@@ -6,28 +6,15 @@ import { Icon } from '@iconify/react';
 // Import components
 import WavesHeader from '../../../components/Header/WavesHeader';
 import UserNav from "../../../components/UserNav/UserNav";
+import ChooseDegreeNav from "../../../components/ChooseDegreeNav/ChooseDegreeNav";
 import AuthContext from "../../../utils/context/AuthContext";
-
 
 function DegreeInfo() {
   return (
     <main className="degreeInfo__wrapper">
         <WavesHeader title="Saukko" secondTitle="Autoalan perustutkinto" fill="#9fc9eb" />
         <section className="degreeInfo__container">
-            <div className="degreeInfo__container--nav">
-                <div>
-                    <Icon icon="iconamoon:number-1-circle-thin" />
-                    <p>Valitse tutkinto</p>
-                </div>
-                <div>
-                    <Icon icon="iconamoon:number-2-circle-thin" />
-                    <p>Valitse tutkinnonosat</p>
-                </div>
-                <div>
-                    <Icon icon="iconamoon:number-3-circle-thin" />
-                    <p>Vahvista pyyntö</p>
-                </div>
-            </div>
+            <ChooseDegreeNav />
             <div className="degreeInfo__container--info"></div>
         </section>
         <UserNav />
