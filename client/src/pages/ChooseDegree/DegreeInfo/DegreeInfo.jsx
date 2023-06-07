dae3f68544e7dc382dd6e44386af69aae3b6e868
hnnav <henna.vahatiitto@gmail.com>
2023-06-07 12:44:32 +0300

    Update page numbers component

--- client/src/pages/ChooseDegree/DegreeInfo/DegreeInfo.jsx
+++ client/src/pages/ChooseDegree/DegreeInfo/DegreeInfo.jsx
@@ -6,7 +6,7 @@ import { Icon } from '@iconify/react';
 // Import components
 import WavesHeader from '../../../components/Header/WavesHeader';
 import UserNav from "../../../components/UserNav/UserNav";
-import ChooseDegreeNav from "../../../components/ChooseDegreeNav/ChooseDegreeNav";
+import PageNumbers from "../../../components/PageNumbers/PageNumbers";
 import AuthContext from "../../../utils/context/AuthContext";
 
 function DegreeInfo() {
@@ -14,7 +14,7 @@ function DegreeInfo() {
     <main className="degreeInfo__wrapper">
         <WavesHeader title="Saukko" secondTitle="Autoalan perustutkinto" fill="#9fc9eb" />
         <section className="degreeInfo__container">
-            <ChooseDegreeNav />
+            <PageNumbers activePage={1}/>
             <div className="degreeInfo__container--info"></div>
         </section>
         <UserNav />
