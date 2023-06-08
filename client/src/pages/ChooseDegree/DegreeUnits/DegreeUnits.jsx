972e6b24195a3f528018921d93c5430a6a45a12b
Henna <57915266+hnnav@users.noreply.github.com>
2023-06-08 14:58:57 +0300

    Add checkbox functionality & styling

--- client/src/pages/ChooseDegree/DegreeUnits/DegreeUnits.jsx
+++ client/src/pages/ChooseDegree/DegreeUnits/DegreeUnits.jsx
@@ -1,5 +1,5 @@
 // Import react packages & dependencies
-import React from "react";
+import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 
 // Import components
@@ -11,6 +11,17 @@ import PageNumbers from "../../../components/PageNumbers/PageNumbers";
 import { units } from "./unitsTempData";
 
 function DegreeUnits() {
+
+  const [checkedUnits, setCheckedUnits] = useState([]);
+
+  const handleCheckboxChange = (unitId) => {
+    if (checkedUnits.includes(unitId)) {
+      setCheckedUnits(checkedUnits.filter((id) => id !== unitId));
+    } else {
+      setCheckedUnits([...checkedUnits, unitId]);
+    }
+  };
+  
   return (
     <main className="degreeUnits__wrapper">
         <WavesHeader title="Saukko" secondTitle="Autoalan perustutkinto" fill="#9fc9eb" />
@@ -30,8 +41,14 @@ function DegreeUnits() {
             <div className="degreeUnits__container--units">
               {units.map((unit, index) => (
                   <div key={unit._id} className="degreeUnits__container--units-unit">
-                    <div className="degreeUnits__container--units-unit-checkbox">
-                      {/* <Icon icon="mdi:tick" color="white" /> */}
+                    <div
+                      className={`degreeUnits__container--units-unit-checkbox ${checkedUnits.includes(unit._id) && 'checked'}`}
+                      onClick={() => handleCheckboxChange(unit._id)}
+                    >
+                      {checkedUnits.includes(unit._id) && (
+                        <Icon icon="mdi:tick" color="white"/>
+                        )}
+                        {console.log(checkedUnits)}
                     </div>
                     <p key={unit._id}><b>{index+1}.</b> {unit.name.fi}</p>
                     <Icon icon="iconamoon:arrow-right-2-light" className="degreeUnits__container--units-unit-arrow" />
