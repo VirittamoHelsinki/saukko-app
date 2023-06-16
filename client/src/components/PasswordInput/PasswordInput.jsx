71aed192cd83f72fc79d677f9c98f5db46a203c9
nedpat <74729837+nedpat@users.noreply.github.com>
2023-06-16 10:33:43 +0300

    Add latest working version of PasswordInput component

--- client/src/components/PasswordInput/PasswordInput.jsx
+++ client/src/components/PasswordInput/PasswordInput.jsx
@@ -2,8 +2,19 @@ import { React, useState } from 'react';
 import useStore from '../../useStore';
 import { Icon } from '@iconify/react';
 
+/**
+ * The PasswordInput component accepts two props: one for a label and one for
+ * a state variable. Usage of state variables is limited to those that are predetermined
+ * and registered within the useStore.js file. The available state variable names are:
+ * "password", "passwordVerify", and "passwordOld"
+ * Example usage:
+ * <PasswordInput value='passwordOld' label='Vanha salasana *' />
+ * <PasswordInput value='password' label='Uusi salasana *' />
+ * <PasswordInput value='passwordVerify' label='Vahvista salasana *' />
+ */
+
 const PasswordInput = (props) => {
-  const { password, setPassword } = useStore();
+  const { setPassword, setPasswordVerify, setPasswordOld } = useStore();
 
   // State variables for password visibility toggling
   const [showPassword, setShowPassword] = useState(false);
@@ -20,20 +31,24 @@ const PasswordInput = (props) => {
         <div className='password__container'>
           <input
             type={showPassword ? 'text' : 'password'}
-            value={password}
             onChange={(e) => {
-              setPassword(e.target.value);
+              if (props.value === 'passwordOld') {
+                setPasswordOld(e.target.value);
+              } else if (props.value === 'passwordVerify') {
+                setPasswordVerify(e.target.value);
+              } else {
+                setPassword(e.target.value);
+              }
             }}
           />
-          {password.length > 0 && (
-            <span className='password-icon' onClick={togglePasswordVisibility}>
-              {showPassword ? (
-                <Icon icon='mdi:eye-off-outline' className='eye-off' />
-              ) : (
-                <Icon icon='mdi:eye-outline' className='eye-on' />
-              )}
-            </span>
-          )}
+
+          <span className='password-icon' onClick={togglePasswordVisibility}>
+            {showPassword ? (
+              <Icon icon='mdi:eye-off-outline' className='eye-off' />
+            ) : (
+              <Icon icon='mdi:eye-outline' className='eye-on' />
+            )}
+          </span>
         </div>
       </div>
     </>
