113ffd0e5c6da453ab62dd29514cf3b960309ef0
nedpat <74729837+nedpat@users.noreply.github.com>
2023-06-19 09:17:24 +0300

    Remove Typography sections from modal

--- client/src/components/NotificationModal/NotificationModal.jsx
+++ client/src/components/NotificationModal/NotificationModal.jsx
@@ -62,6 +62,9 @@ export default function CustomizedDialogs() {
         onClose={handleClose}
         aria-labelledby='customized-dialog-title'
         open={open}
+        sx={{
+          color: 'success.main',
+        }}
       >
         <BootstrapDialogTitle
           id='customized-dialog-title'
@@ -74,16 +77,6 @@ export default function CustomizedDialogs() {
             Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
             dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta
             ac consectetur ac, vestibulum at eros.
-          </Typography>
-          <Typography gutterBottom>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
-            Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor
-            auctor.
-          </Typography>
-          <Typography gutterBottom>
-            Aenean lacinia bibendum nulla sed consectetur. Praesent commodo
-            cursus magna, vel scelerisque nisl consectetur et. Donec sed odio
-            dui. Donec ullamcorper nulla non metus auctor fringilla.
           </Typography>
         </DialogContent>
       </BootstrapDialog>
