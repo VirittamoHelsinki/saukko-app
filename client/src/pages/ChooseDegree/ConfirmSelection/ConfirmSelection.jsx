3f029c6241eec20ad163723fd324ef3648a2c8f6
Henna <57915266+hnnav@users.noreply.github.com>
2023-06-20 13:04:24 +0300

    Render units ids

--- client/src/pages/ChooseDegree/ConfirmSelection/ConfirmSelection.jsx
+++ client/src/pages/ChooseDegree/ConfirmSelection/ConfirmSelection.jsx
@@ -6,16 +6,36 @@ import { useNavigate } from 'react-router-dom';
 import WavesHeader from '../../../components/Header/WavesHeader';
 import UserNav from '../../../components/UserNav/UserNav';
 import PageNumbers from '../../../components/PageNumbers/PageNumbers';
+import SelectUnit from '../../../components/SelectUnit/SelectUnit';
 import Button from '../../../components/Button/Button';
+import { degrees } from '../SearchPage/tempData';
+import useUnitsStore from '../../../unitsStore';
 
 function ConfirmSelection() {
   const navigate = useNavigate();
 
+  // Temp data
+  const degree = {
+    name: "Autoalan perustutkinto"
+  }
+
+  // Get checked units from unitsStore
+  const checkedUnits = useUnitsStore((state) => state.checkedUnits);
+
   return (
     <main className='confirmSelection__wrapper'>
-        <WavesHeader title='Saukko' secondTitle='Autoalan perustutkinto' /> {/* Make secondTitle dynamic later */}
+        <WavesHeader title='Saukko' secondTitle={degree.name} />
         <section className='confirmSelection__container'>
           <PageNumbers activePage={3}/>
+          <h1 className='confirmSelection__container--title'>Vahvista pyyntö</h1>
+          <p className='confirmSelection__container--desc'>{`Olet hakemassa ${degree.name} osien suoria näyttöjä`} </p>
+          <h1 className='confirmSelection__container--secondtitle'>Valitsemasi tutkinnonosat</h1>
+          <div className='confirmSelection__container--units'>
+            {console.log(console.log('checked units confirm selection page: ', checkedUnits))}
+            {checkedUnits.map((unitId) => (
+              <p>{unitId}</p>
+            ))}
+          </div>
         </section>
         <UserNav />
     </main>
