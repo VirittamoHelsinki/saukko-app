873472511581b94f5c602f3087f7f8ddf5125598
sericakitty <serica.suhonen@hotmail.com>
2023-06-21 10:59:34 +0300

    add nodemailer functions

--- server/mailer.js
+++ server/mailer.js
@@ -1,23 +1,40 @@
-
 const nodemailer = require('nodemailer');
-const config = require('../src/utils/config')
+const config = require('./utils/config');
 
-const setup = (user) => {
-  return nodemailer.createTransport({})
+// setup email transport
+const setup = () => {
+  return nodemailer.createTransport({
+    service: config.EMAIL_SERVICE,
+    port: config.EMAIL_SERVICE_PORT,
+    auth: {
+      user: config.EMAIL_SERVICE_USER,
+      pass: config.EMAIL_SERVICE_PASSWORD
+    }
+  })
 }
 
-// This function is not yet in use
+// send email to user with reset password link
 const sendResetPasswordEmail = (user) => {
-  const transport = setup(user);
-  const emailObject = {}
-  transport.sendMail(emailObject, (err, info) => {
-    if (err) {
-      console.log(err.message);
-      return false
-    } 
-    console.log(info);
-    return true
-  })
+  
+  // create transport object
+  const transport = setup();
+  
+  // create email object
+  const emailObject = {
+    from: config.EMAIL_SERVICE_FROM,
+    to: user.email,
+    subject: 'Reset your password',
+    text: `
+      Hi ${user.name},
+
+      To reset your password, 
+      
+      click this link: ${user.generateResetPasswordLink()}
+    `
+  }
+
+  // send email
+  transport.sendMail(emailObject)
 }
 
 module.exports = { sendResetPasswordEmail }
