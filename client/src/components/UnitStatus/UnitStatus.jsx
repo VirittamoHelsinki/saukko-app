d6ba98e0a88776b90a0b2983f2b8ba66a5eeb57e
Mirazum <mira.sust@gmail.com>
2023-06-21 10:52:55 +0300

    Update status according to the user role

--- client/src/components/UnitStatus/UnitStatus.jsx
+++ client/src/components/UnitStatus/UnitStatus.jsx
@@ -1,9 +1,9 @@
-import greenicone from '../../assets/circle-green.jpg';
-import redicone from '../../assets/circle-red.jpg';
-import yellowicone from '../../assets/circle-yellow.jpg';
-import blueicone from '../../assets/circle-blue.jpg';
-import blackicone from '../../assets/circle-black.jpg';
-import right from '../../assets/angle-right.jpg';
+import greenicone from '../../assets/circle-green.svg';
+import redicone from '../../assets/circle-red.svg';
+import yellowicone from '../../assets/circle-yellow.svg';
+import blueicone from '../../assets/circle-blue.svg';
+import blackicone from '../../assets/circle-black.svg';
+import rightAngle from '../../assets/angle-right.svg';
 
 import React from 'react';
 import { Link } from 'react-router-dom';
@@ -12,41 +12,55 @@ const UnitStatus = ({ status, subheader, link }) => {
   let header, backgroundColor, img;
 
   if (status === 1) {
-    img = <img src={greenicone} alt="" />;
+    img = <img src={greenicone} alt="Icone" />;
     header = 'Hyväksytty';
     backgroundColor = '#E2F5F3';
   } else if (status === 2) {
-    img = <img src={yellowicone} alt="" />;
+    img = <img src={yellowicone} alt="Icone" />;
     header = 'Käsittelyssä';
     backgroundColor = '#FFF4B4';
   } else if (status === 3) {
-    img = <img src={redicone} alt="" />;
+    img = <img src={yellowicone} alt="Icone" />;
+    header = 'Valmis tarkistettavaksi';
+    backgroundColor = '#FFF4B4';
+  }
+  else if (status === 4) {
+    img = <img src={redicone} alt="Icone" />;
     header = 'Täydennettävä';
     backgroundColor = '#F6E2E6';
-  } else if (status === 4) {
-    img = <img src={blueicone} alt="" />;
+  } else if (status === 5) {
+    img = <img src={blueicone} alt="Icone" />;
     header = 'Aloitettu';
     backgroundColor = '#E5EFF8';
-  } else if (status === 5) {
-    img = <img src={blackicone} alt="" />;
+  } else if (status === 6) {
+    img = <img src={blackicone} alt="Icone" />;
     header = 'Aloittamatta';
     backgroundColor = '#efeff0';
   }
 
+
+
   return (
-    <div style={{ backgroundColor }}>
-      <div>
-        <Link to={link}>
-          <div>{img}</div>
-        </Link>
+    <main className='unitstatus-wrapper' style={{ backgroundColor }}>
+      <div className='unitstatus'>
+        <div>{img}</div>
         <h1>{header}</h1>
+
+        <Link to={link}><img className="right-angle-icon" src={rightAngle} alt="Angle Icone" /></Link>
+
       </div>
+
       {subheader && <h2>{subheader}</h2>}
-    </div>
+
+    </main>
   );
 };
 
+
+
 export default UnitStatus;
+
+
 
 
 
