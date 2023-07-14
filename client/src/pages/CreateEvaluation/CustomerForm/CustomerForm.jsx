e3c0ea4f2546aa03ef5ed23c43d0aecbd687a968
Henna <57915266+hnnav@users.noreply.github.com>
2023-07-14 11:03:27 +0300

    Add more starter code

--- client/src/pages/CreateEvaluation/CustomerForm/CustomerForm.jsx
+++ client/src/pages/CreateEvaluation/CustomerForm/CustomerForm.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import PageNumbers from '../../../components/PageNumbers/PageNumbers';
+import WavesHeader from '../../../components/Header/WavesHeader';
+import UserNav from '../../../components/UserNav/UserNav';
+import PageNavigationButtons from '../../../components/PageNavigationButtons/PageNavigationButtons';
 
 function CustomerForm() {
+  const navigate = useNavigate();
+
   return (
     <main className='customerForm__wrapper'>
+      <WavesHeader title='Saukko' secondTitle='Suorituksen aktivoiminen' />
       <section className='customerForm__container'>
-        <h1>Form</h1>
+        <PageNumbers activePage={1}/>
+        <h1>Lisää asiakkaan tiedot</h1>
+        <PageNavigationButtons /* handleBack={() => navigate(`/`)} */ handleForward={() => navigate(`/evaluation-workplace`)} forwardButtonText={'Seuraava'}/>
       </section>
+      <UserNav />
     </main>
   );
 };
