976f10498c8b744ad7dfe4480cd21151c15bfc27
nedpat <74729837+nedpat@users.noreply.github.com>
2023-07-27 14:31:41 +0300

    Remove arrow icons from SelectUnit component

--- client/src/components/SelectUnit/SelectUnit.jsx
+++ client/src/components/SelectUnit/SelectUnit.jsx
@@ -4,13 +4,12 @@ import { Icon } from '@iconify/react';
 import useUnitsStore from '../../unitsStore';
 import DegreeContext from '../../utils/context/DegreeContext';
 
-
 function SelectUnit({ unit, allUnits }) {
   const navigate = useNavigate();
 
   // Get degree from degreeContext
   const { degree } = useContext(DegreeContext);
-  
+
   // Check / uncheck unit using toggleUnit function from UnitsStore
   const toggleUnit = useUnitsStore((state) => state.toggleUnit);
   const handleCheckboxChange = () => {
@@ -23,12 +22,12 @@ function SelectUnit({ unit, allUnits }) {
 
   // Number units
   const getUnitNumber = (id) => {
-    const foundUnit = allUnits && allUnits.find(unit => unit._id === id);
+    const foundUnit = allUnits && allUnits.find((unit) => unit._id === id);
     if (foundUnit) {
       return allUnits.indexOf(foundUnit) + 1;
     }
   };
-  
+
   return (
     <div
       key={unit._id}
@@ -42,19 +41,13 @@ function SelectUnit({ unit, allUnits }) {
         }`}
         onClick={handleCheckboxChange}
       >
-        {checkedUnits.includes(unit) && <Icon icon="mdi:tick" color="white" />}
+        {checkedUnits.includes(unit) && <Icon icon='mdi:tick' color='white' />}
       </div>
       <p>
         <b>{getUnitNumber(unit._id)}.</b> {unit.name.fi}
       </p>
-      <Icon 
-        icon="iconamoon:arrow-right-2-light" 
-        className="selectUnit__container--units-unit-arrow" 
-        onClick={() => navigate(`/degrees/${degree._id}/units/${unit._id}`)}
-      />
     </div>
-  )
+  );
 }
 
 export default SelectUnit;
-
