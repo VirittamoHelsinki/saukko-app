04f61974296478be96fffc634cbc362cd91df6e1
Henna <57915266+hnnav@users.noreply.github.com>
2023-07-28 13:16:47 +0300

    Refactor degrees fetch

--- client/src/api/degree.js
+++ client/src/api/degree.js
@@ -3,3 +3,15 @@ import axios from 'axios'
 const baseURL = process.env.REACT_APP_BACKEND_URL
 
 const middleURL = '/api'
+
+const fetchAll = async () => {
+  const response = await axios.get(`${baseURL}${middleURL}/degrees`)
+  return response
+}
+
+const fetchById = async (id) => {
+  const response = await axios.get(`${baseURL}${middleURL}/degree/${id}`)
+  return response;
+};
+
+export { fetchAll, fetchById }
