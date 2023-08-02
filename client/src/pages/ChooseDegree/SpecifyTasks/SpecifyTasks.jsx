bc457039d09fb41718dbd2af9c654951cfaeeb7f
nedpat <74729837+nedpat@users.noreply.github.com>
2023-08-02 15:23:37 +0300

    Install MUI carousel stepper

--- client/src/pages/ChooseDegree/SpecifyTasks/SpecifyTasks.jsx
+++ client/src/pages/ChooseDegree/SpecifyTasks/SpecifyTasks.jsx
@@ -5,10 +5,45 @@ import { useNavigate, useParams } from 'react-router-dom';
 // Import components
 import WavesHeader from '../../../components/Header/WavesHeader';
 import UserNav from '../../../components/UserNav/UserNav';
+import { useTheme } from '@mui/material/styles';
 import Stepper from '../../../components/Stepper/Stepper';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
 import PageNavigationButtons from '../../../components/PageNavigationButtons/PageNavigationButtons';
 import useUnitsStore from '../../../unitsStore';
 import DegreeContext from '../../../utils/context/DegreeContext';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = [
+  {
+    label: 'San Francisco – Oakland Bay Bridge, United States',
+    imgPath:
+      'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bird',
+    imgPath:
+      'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+  {
+    label: 'Bali, Indonesia',
+    imgPath:
+      'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250',
+  },
+  {
+    label: 'Goč, Serbia',
+    imgPath:
+      'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
+  },
+];
 
 function SpecifyTasks() {
   const navigate = useNavigate();
@@ -33,6 +68,23 @@ function SpecifyTasks() {
     'Vahvista',
   ];
 
+  // Dots Stepper
+  const theme = useTheme();
+  const [activeStep, setActiveStep] = useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
   return (
     <main className='confirmSelection__wrapper'>
       <WavesHeader title='Saukko' secondTitle={degreeFound && degree.name.fi} />
@@ -43,6 +95,79 @@ function SpecifyTasks() {
           label={labelStepper}
           url={`/degrees/${degree._id}/units/tasks`}
         />
+        <Box sx={{ maxWidth: 400, flexGrow: 1 }}>
+          <MobileStepper
+            steps={maxSteps}
+            position='static'
+            activeStep={activeStep}
+            nextButton={
+              <Button
+                size='small'
+                onClick={handleNext}
+                disabled={activeStep === maxSteps - 1}
+              >
+                Seuraava
+                {theme.direction === 'rtl' ? (
+                  <KeyboardArrowLeft />
+                ) : (
+                  <KeyboardArrowRight />
+                )}
+              </Button>
+            }
+            backButton={
+              <Button
+                size='small'
+                onClick={handleBack}
+                disabled={activeStep === 0}
+              >
+                {theme.direction === 'rtl' ? (
+                  <KeyboardArrowRight />
+                ) : (
+                  <KeyboardArrowLeft />
+                )}
+                Edellinen
+              </Button>
+            }
+          />
+          <Paper
+            square
+            elevation={0}
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              height: 50,
+              pl: 2,
+              bgcolor: 'background.default',
+            }}
+          >
+            <Typography>{images[activeStep].label}</Typography>
+          </Paper>
+          <AutoPlaySwipeableViews
+            axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+            index={activeStep}
+            onChangeIndex={handleStepChange}
+            enableMouseEvents
+          >
+            {images.map((step, index) => (
+              <div key={step.label}>
+                {Math.abs(activeStep - index) <= 2 ? (
+                  <Box
+                    component='img'
+                    sx={{
+                      height: 255,
+                      display: 'block',
+                      maxWidth: 400,
+                      overflow: 'hidden',
+                      width: '100%',
+                    }}
+                    src={step.imgPath}
+                    alt={step.label}
+                  />
+                ) : null}
+              </div>
+            ))}
+          </AutoPlaySwipeableViews>
+        </Box>
 
         <PageNavigationButtons
           handleBack={() =>
