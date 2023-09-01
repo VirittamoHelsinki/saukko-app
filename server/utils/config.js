a5873d9407cc50198b090599a7ace63cc800248b
Veikko Laine <veikko.laine@gmail.com>
2023-09-01 13:38:12 +0300

    Added automatic testing environment and its configuration using jest supertest and cross-env. Added one automatic test for user login.

--- server/utils/config.js
+++ server/utils/config.js
@@ -2,7 +2,9 @@
 require('dotenv').config()
 
 // import MONGODB_URI from .env
-const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+    ? process.env.TEST_MONGODB_URI
+    : process.env.MONGODB_URI
 
 // import PORT from .env
 const PORT = process.env.PORT
