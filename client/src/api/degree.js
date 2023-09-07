dd8f3303c7590ebb56ec752f12ee2bdd00d70f47
Veikko Laine <veikko.laine@gmail.com>
2023-09-07 12:00:55 +0300

    Added api calls to api/degree.js and api/workplace.js

--- client/src/api/degree.js
+++ client/src/api/degree.js
@@ -4,14 +4,28 @@ const baseURL = process.env.REACT_APP_BACKEND_URL
 
 const middleURL = '/api'
 
+// Fetch all degrees from ePerusteet.
 const fetchAll = async () => {
   const response = await axios.get(`${baseURL}${middleURL}/degrees`)
   return response
 }
 
-const fetchById = async (id) => {
+// Fetch a degree by id from ePerusteet.
+const fetchById = async id => {
   const response = await axios.get(`${baseURL}${middleURL}/degree/${id}`)
   return response;
-};
+}
 
-export { fetchAll, fetchById }
+// Fetch all degrees from internal saukko database.
+const fetchAllInternal = async () => {
+  const response = await axios.get(`${baseURL}${middleURL}/internal/degrees`)
+  return response
+}
+
+// Post a degree to internal saukko database.
+const post = async degree => {
+  const response = await axios.post(`${baseURL}${middleURL}/internal/degrees`, degree)
+  return response
+}
+
+export { fetchAll, fetchById, fetchAllInternal, post }
--- client/src/api/workplace.js
+++ client/src/api/workplace.js
@@ -4,9 +4,40 @@ const baseURL = process.env.REACT_APP_BACKEND_URL
 
 const middleURL = '/api'
 
+// Fetch company data from the an external API.
 const fetchCompanyData = async (businessID) => {
   const response = await fetch(`${baseURL}${middleURL}/business/${businessID}`);
   return response
 };
 
-export { fetchCompanyData }
+// Fetch all workplaces from internal saukko database.
+const fetchAllInternal = async () => {
+  const response = await axios.get(`${baseURL}${middleURL}/workplace`)
+  return response
+}
+
+// Fetch a workplace by id from internal saukko database.
+const fetchInternalById = async id => {
+  const response = await axios.get(`${baseURL}${middleURL}/workplace/${id}`)
+  return response;
+}
+
+// Post a workplace to internal saukko database.
+const post = async workplace => {
+  const response = await axios.post(`${baseURL}${middleURL}/workplace`, workplace)
+  return response
+}
+
+// Update a workplace in internal saukko database.
+const update = async (id, workplace) => {
+  const response = await axios.put(`${baseURL}${middleURL}/workplace/${id}`, workplace)
+  return response
+}
+
+// Remove a workplace from internal saukko database.
+const remove = async (id) => {
+  const response = await axios.delete(`${baseURL}${middleURL}/workplace/${id}`)
+  return response
+}
+
+export { fetchCompanyData, fetchAllInternal, fetchInternalById, post, update, remove }
