7525d635da0586d424c59c2f7ca723a01980685a
Veikko Laine <veikko.laine@gmail.com>
2023-09-14 12:57:07 +0300

    Added get internal degree with id route to backend.

--- server/routers/degreeRouter.js
+++ server/routers/degreeRouter.js
@@ -17,6 +17,17 @@ degreeRouter.get('/internal/degrees', async (req, res) => {
     }
   });
 
+  // GET all degrees
+degreeRouter.get('/internal/degree/:id', async (req, res) => {
+  try {
+    const degree = await Degree.findById(req.params.id);
+    res.status(200).json(degree);
+  } catch (error) {
+    console.error('Error fetching degrees:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
   // POST a new degree
 degreeRouter.post('/internal/degrees', async (req, res) => {
     try {
