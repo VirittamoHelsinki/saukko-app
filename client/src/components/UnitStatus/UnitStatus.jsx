267da710bbd7a5eb7bca54ac762e1566851f30bc
nedpat <74729837+nedpat@users.noreply.github.com>
2023-10-11 08:06:00 +0300

    Removing the täydennettävää element from the list of client tasks

--- client/src/components/UnitStatus/UnitStatus.jsx
+++ client/src/components/UnitStatus/UnitStatus.jsx
@@ -12,38 +12,41 @@ const UnitStatus = ({ status, subheader, link }) => {
   let header, backgroundColor, img;
 
   if (status === 1) {
-    img = <img src={greenicone} alt="Icone" />;
+    img = <img src={greenicone} alt='Icone' />;
     header = 'Hyväksytty';
     backgroundColor = '#E2F5F3';
   } else if (status === 2) {
-    img = <img src={yellowicone} alt="Icone" />;
+    img = <img src={yellowicone} alt='Icone' />;
     header = 'Käsittelyssä';
     backgroundColor = '#FFF4B4';
   } else if (status === 3) {
-    img = <img src={yellowicone} alt="Icone" />;
+    img = <img src={yellowicone} alt='Icone' />;
     header = 'Valmis tarkistettavaksi';
     backgroundColor = '#FFF4B4';
-  }
-  else if (status === 4) {
-    img = <img src={redicone} alt="Icone" />;
+  } else if (status === 4) {
+    img = <img src={redicone} alt='Icone' />;
     header = 'Täydennettävä';
     backgroundColor = '#F6E2E6';
   } else if (status === 5) {
-    img = <img src={blueicone} alt="Icone" />;
+    img = <img src={blueicone} alt='Icone' />;
     header = 'Aloitettu';
     backgroundColor = '#E5EFF8';
   } else if (status === 6) {
-    img = <img src={blackicone} alt="Icone" />;
+    img = <img src={blackicone} alt='Icone' />;
     header = 'Aloittamatta';
     backgroundColor = '#efeff0';
   }
 
   return (
-    <main className='unitstatus-wrapper' style={{ backgroundColor }} onClick={() => navigate(link)}>
+    <main
+      className='unitstatus-wrapper'
+      style={{ backgroundColor }}
+      onClick={() => navigate(link)}
+    >
       <div className='unitstatus'>
         <div className='icone-img-style'>{img}</div>
         <h1>{header}</h1>
-        <img className="icone-img-style" src={rightAngle} alt="Angle Icone" />
+        <img className='icone-img-style' src={rightAngle} alt='Angle Icone' />
       </div>
       {subheader && <h2>{subheader}</h2>}
     </main>
