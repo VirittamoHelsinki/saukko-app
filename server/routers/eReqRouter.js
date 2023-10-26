e313b1ae2f229ce3ca659c6cea40a5ebc7e52449
juhis96 <juha-matti.raty@hotmail.com>
2023-10-26 15:10:18 +0300

    changed fetch to axios

--- server/routers/eReqRouter.js
+++ server/routers/eReqRouter.js
@@ -1,10 +1,9 @@
-
 const router = require("express").Router();
-const jp = require('jsonpath')
-
+const jp = require('jsonpath');
+ 
 //ePerusteet API URL
 const ePerusteAPI = 'https://eperusteet.opintopolku.fi/eperusteet-service/api/external';
-
+ 
 //Function for fetching data from URL
 async function fetchData(url) {
   try {
@@ -19,7 +18,7 @@ async function fetchData(url) {
     throw new Error('Failed to fetch data');
   }
 }
-
+ 
 router.get(['/business/:id', '/external/business/:id'], async (req, res) => {
   try {
     //Helsingin kaupuki is hard-coded, because it's not found in the API
@@ -33,13 +32,13 @@ router.get(['/business/:id', '/external/business/:id'], async (req, res) => {
     res.status(500).json({ error: "Business not found" });
   }
 });
-
+ 
 //Fetch all degrees from ePerusteet
 router.get(['/degrees', '/external/degrees'], async (req, res) => {
   try {
     const degreeList = [];
     const degrees = await fetchData(`${ePerusteAPI}/perusteet?sivukoko=100000&voimassa=true`);
-
+ 
     degrees.data.forEach(degree => {
       diaryNumber = String(degree.diaarinumero);
       diaryYear = parseInt(diaryNumber.substring(diaryNumber.length - 4));
@@ -47,65 +46,67 @@ router.get(['/degrees', '/external/degrees'], async (req, res) => {
         degreeList.push({ _id: degree.id, diaryNumber: degree.diaarinumero, eduCodeValue: degree.koulutukset[0].koulutuskoodiArvo, name: { fi: degree.nimi.fi, sv: degree.nimi.sv, en: degree.nimi.en } });
       }
     });
-
+ 
     res.json(degreeList);
-
+ 
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
-
-
+ 
+ 
 //Fetch degree information from ePerusteet
 router.get(['/degree/:id', '/external/degree/:id'], async (req, res) => {
   try {
-    const degreeData = await fetchData(`${ePerusteAPI}/peruste/${req.params.id}`);
-    const degreeUnits = await fetchData(`${ePerusteAPI}/peruste/${req.params.id}/tutkinnonOsat`);
+    const degreeId = req.params.id;
+    console.log(`Fetching data for degree with ID: ${degreeId}`);
+   
+    const degreeData = await fetchData(`${ePerusteAPI}/peruste/${degreeId}`);
+    if (!degreeData) throw new Error('Degree data not found');
+   
+    const degreeUnits = await fetchData(`${ePerusteAPI}/peruste/${degreeId}/tutkinnonOsat`);
+    if (!degreeUnits) throw new Error('Degree units not found');
+   
     let unitList = [];
-    let modulex;
-
+ 
     degreeUnits.forEach(unit => {
       let modules = jp.query(unit, '$..arviointi.arvioinninKohdealueet[*]');
-      modulex = modules;
-
-      let assessmentList = [];
-
-      /*modules.forEach(module => {
-        let criteria = module.arvioinninKohteet[0].osaamistasonKriteerit[0].kriteerit;
-        let criteriaList = [];
-
-        criteria.forEach(criterion => {
-          criteriaList.push({_id: criterion._id, fi: criterion.fi, sv: criterion.sv, en: criterion.en});
-        });
-        
-
-      assessmentList.push({_id: module.koodi.id, name: {fi: module.otsikko.fi, sv: module.otsikko.sv, en: module.otsikko.en}, criteria: criteriaList});
-      });*/
-
-      unitList.push({ _id: unit.id, name: { fi: unit.nimi.fi, sv: unit.nimi.sv, en: unit.nimi.en }/*, assessments: assessmentList*/ });
-
+      // Log module info to the console for debugging
+      console.log(`Modules for unit ${unit.id}:`, modules);
+ 
+      unitList.push({ _id: unit.id, name: { fi: unit.nimi.fi, sv: unit.nimi.sv, en: unit.nimi.en } });
     });
-
-    const degree = {
-      _id: req.params.id,
-      name: { fi: degreeData.nimi.fi, sv: degreeData.nimi.sv, en: degreeData.nimi.en },
-      eduCodeValue: degreeData.koulutukset[0].koulutuskoodiArvo,
-      diaryNumber: degreeData.diaarinumero,
-      regulationDate: degreeData.paatospvm,
-      transitionEnds: degreeData.siirtymaPaattyy,
-      validFrom: degreeData.voimassaoloAlkaa,
-      expiry: degreeData.voimassaoloLoppuu,
-      examInfoURL: `https://eperusteet.opintopolku.fi/#/fi/ammatillinen/${req.params.id}/tiedot`,
-      description: { fi: degreeData.kuvaus.fi, sv: degreeData.kuvaus.sv, en: degreeData.kuvaus.en },
-      units: unitList,
-    };
-
-    res.json(degree);
-
+ 
+  const degree = {
+  _id: degreeId,
+  name: {
+    fi: degreeData.nimi?.fi || 'N/A',
+    sv: degreeData.nimi?.sv || 'N/A',
+    en: degreeData.nimi?.en || 'N/A'
+  },
+  units: unitList,
+  eduCodeValue: degreeData.koulutukset?.[0]?.koulutuskoodiArvo || 'N/A',
+  diaryNumber: degreeData.diaarinumero || 'N/A',
+  regulationDate: degreeData.paatospvm || 'N/A',
+  transitionEnds: degreeData.siirtymaPaattyy || 'N/A',
+  validFrom: degreeData.voimassaoloAlkaa || 'N/A',
+  expiry: degreeData.voimassaoloLoppuu || 'N/A',
+  examInfoURL: `https://eperusteet.opintopolku.fi/#/fi/ammatillinen/${degreeId}/tiedot`,
+  description: {
+    fi: degreeData.kuvaus?.fi || 'N/A',
+    sv: degreeData.kuvaus?.sv || 'N/A',
+    en: degreeData.kuvaus?.en || 'N/A'
+  },
+};
+ 
+    console.log('Degree info:', degree);
+    res.status(200).json(degree);
+ 
   } catch (error) {
-    res.status(500).json({ error: error.message });
+    console.error('Error fetching degree info:', error); // Log the error to the console
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
-
-
+ 
+ 
 module.exports = router
