f908b151c71d850677738ceb30d08b60795d8cd0
Closan Josephine <closjos@digitalentshelsinki.internal>
2024-01-16 10:55:45 +0200

    create functionality for add ATV and criteria

--- client/src/components/CriteriaModal/CriteriaModal.jsx
+++ client/src/components/CriteriaModal/CriteriaModal.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import { styled } from '@mui/material/styles';
 import Dialog from '@mui/material/Dialog';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
 import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
+import CancelOutlinedIcon from '@mui/icons-material/CancelOutlined';
+import { Button } from '@mui/material';
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   '& .MuiDialogContent-root': {
@@ -34,76 +36,190 @@ function BootstrapDialogTitle(props) {
             color: (theme) => theme.palette.grey[500],
           }}
         >
-          <CloseIcon />
+          <CancelOutlinedIcon />
         </IconButton>
       ) : null}
     </DialogTitle>
   );
 }
 
-export default function CriteriaModal({ open, handleClose}) {
+export default function CriteriaModal({ open, handleClose, onSave }) {
   let bgColor = '#e5eff8';
   let color = '#0062b9';
+  const [inputValueTitle, setInputValueTitle] = useState('');
+  const [inputValueCriteria, setInputValueCriteria] = useState('');
+
+  const handleInputChange = (event, inputField) => {
+    const value = event.target.value;
+    
+    if (inputField === 1) {
+      setInputValueTitle(value);
+    } else if (inputField === 2) {
+      setInputValueCriteria(value);
+    }
+  };
+
+  const handleSubmit = () => {
+    console.log('Submitted value:', inputValueTitle, inputValueCriteria);
+    onSave(inputValueTitle, inputValueCriteria);
+    setInputValueTitle('');
+    setInputValueCriteria('');
+  };
 
   return (
-    <div>
-      <BootstrapDialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby='customized-dialog-title'
+    <BootstrapDialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby='customized-dialog-title'
+      fullWidth
+      maxWidth='sm'
+      sx={{
+        '& .MuiDialog-paper': {
+          background: bgColor,
+          borderLeft: 'solid 5px',
+          color,
+          width: '100%',
+          maxWidth: '500px',
+          '@media (min-width:600px)': {
+            maxWidth: '800px', // adjust this value as needed
+          },
+        },
+      }}
+    >
+      <IconButton
+        aria-label='close'
+        onClick={handleClose}
         sx={{
-          '& .MuiDialog-paper': {
-            background: bgColor,
-            borderLeft: 'solid 8px',
-            color,
-          },
-          '& .MuiDialogTitle-root': {
-            fontSize: '0.875 rem',
-            fontWeight: '700',
-          },
+          position: 'absolute',
+          right: 8,
+          top: 8,
+          color: 'black',
         }}
       >
-        <IconButton
-          aria-label='close'
-          onClick={handleClose}
+        <CancelOutlinedIcon />
+      </IconButton>
+      <BootstrapDialogTitle
+        variant='h6'
+        component='h6'
+        fontWeight='bold'
+        sx={{ fontSize: '17px' }}
+      >
+        Lisää ammattitaitovaatimusta
+      </BootstrapDialogTitle>
+      <DialogContent
+        sx={{
+          marginLeft: '11px',
+        }}
+      >
+        <Typography
+          variant='subtitle1'
+          fontWeight='bold'
+          marginBottom={1}
+          sx={{ fontSize: '12px' }}
+        >
+          Osan tutkinnon nimi
+        </Typography>
+        <Typography
+          variant='subtitle1'
+          fontWeight='bold'
+          sx={{ fontSize: '12px' }}
+        >
+          Ammattitaitovaatimuksen nimi
+        </Typography>
+        <TextField
+          value={inputValueTitle}
+          onChange={(e) => handleInputChange(e, 1)}
+          id='outlined-multiline-static'
+          backgroundColor='#FFFFFF'
+          border='black 2px solid'
+          fontSize='10px'
+          multiline
+          variant='outlined'
           sx={{
-            position: 'absolute',
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
+            width: '95%',
+
+            borderColor: 'black',
+            borderRadius: '0',
+            backgroundColor: 'white',
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderColor: 'black',
+                borderWidth: '2px',
+                borderRadius: '0',
+              },
+              '&:hover fieldset': {
+                borderColor: 'black',
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: 'black',
+              },
+            },
           }}
         >
-          <CloseIcon />
-        </IconButton>
-        <BootstrapDialogTitle
-          id='customized-dialog-title'
-          variant='h6'
-          component='h2'
-          textAlign='center'
+          <Typography padding='1rem'></Typography>
+        </TextField>
+        <Typography
+          variant='subtitle1'
           fontWeight='bold'
+          marginTop={1}
+          sx={{ fontSize: '12px' }}
         >
-          Kritteerit
-        </BootstrapDialogTitle>
-        <DialogContent id='modal-modal-description'>
-          <Box
-            backgroundColor='#FFFFFF'
-            border='black 2px solid'
-            margin='0 1rem 3rem'
-          >
-            <Typography padding='1rem'>
-              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
-              <p>
-                Minima enim dignissimos aliquam et animi accusantium illo
-                laboriosam pariatur inventore!
-              </p>
-              <p>
-                Fugit ullam debitis hic doloremque aperiam eum temporibus
-                deserunt amet similique.
-              </p>
-            </Typography>
-          </Box>
-        </DialogContent>
-      </BootstrapDialog>
-    </div>
+          Kriteerit
+        </Typography>
+        <TextField
+          value={inputValueCriteria}
+          onChange={(e) => handleInputChange(e, 2)}
+          id='outlined-multiline-static'
+          multiline
+          variant='outlined'
+          sx={{
+            width: '95%',
+            borderColor: 'black',
+            borderRadius: '0',
+            backgroundColor: 'white',
+            '& .MuiOutlinedInput-root': {
+              '& fieldset': {
+                borderColor: 'black',
+                borderWidth: '2px',
+                borderRadius: '0',
+              },
+              '&:hover fieldset': {
+                borderColor: 'black',
+              },
+              '&.Mui-focused fieldset': {
+                borderColor: 'black',
+              },
+            },
+          }}
+        >
+          <Typography padding='1rem'></Typography>
+        </TextField>
+      </DialogContent>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          width: '100%',
+          marginBottom: '20px',
+        }}
+      >
+        <Button
+          onClick={handleSubmit}
+          sx={{
+            textTransform: 'none',
+            backgroundColor: '#0000BF',
+            width: '99px',
+            height: '56px',
+            color: '#FFFFFF',
+            borderRadius: '0',
+            '&:hover': {
+              backgroundColor: '#0000BF',
+            },
+          }}
+        >
+          Tallenna
+        </Button>
+      </Box>
+    </BootstrapDialog>
   );
 }
