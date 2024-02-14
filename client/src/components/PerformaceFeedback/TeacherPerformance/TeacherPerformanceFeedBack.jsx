7403265ed8f2b888c6c58db05fcf3dc1489a8422
Uehara Tomoko <uehatom@digitalentshelsinki.internal>
2024-02-14 06:34:40 +0200

    test

--- client/src/components/PerformaceFeedback/TeacherPerformance/TeacherPerformanceFeedBack.jsx
+++ client/src/components/PerformaceFeedback/TeacherPerformance/TeacherPerformanceFeedBack.jsx
@@ -4,6 +4,8 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import AuthContext from '../../../store/context/AuthContext';
+import InternalApiContext from '../../../store/context/InternalApiContext';
+import useEvaluationStore from '../../../store/zustand/evaluationStore';
 
 const TeacherPerformanceFeedBack = ({
   setSelectedValues,
@@ -11,32 +13,41 @@ const TeacherPerformanceFeedBack = ({
   setSelectedUnitId,
 }) => {
   const [selectedRadio, setSelectedRadio] = useState({});
+
+  const {evaluation} = useContext(InternalApiContext);
+  
   const auth = useContext(AuthContext);
   const user = auth.user;
 
-  const handleRadioChange = (e, unit, info, value) => {
+  if(user&&user.role !=='teacher'){
+    return null;
+  }
+  
+  const handleRadioChange = (e, unit, info) => {
     console.log('🚀 ~ handleRadioChange ~ unit:', unit);
     setSelectedUnitId(unit._id); // This is the unit id
     console.log('🚀 ~ handleRadioChange ~ unit._id:', unit._id);
+
+    if(!unit || !unit._id){
+      console.log('Unit or unit._id is undefined');
+      return;
+    }
+
+    const value = e.target.value;
     setSelectedRadio((prevValues) => ({
       ...prevValues,
       [info]: prevValues[info] === value ? '' : value,
     }));
-    if (e.target) {
-      if (selectedRadio === e.target.value) {
-        e.target.checked = false;
-        setSelectedRadio('');
-        setSelectedValues(0);
-      } else {
-        setSelectedRadio(e.target.value);
-        if (e.target.value === 'Osaa ohjatusti') {
-          setSelectedValues(1);
-        } else if (e.target.value === 'Osaa itsenäisesti') {
-          setSelectedValues(2);
-        }
-      }
-      console.log(e.target.value);
+    if (value === 'Osaa ohjatusti') {
+      setSelectedValues(1);
+    } else if (value === 'Osaa itsenäisesti'){
+      setSelectedValues(2);
+    } else {
+      setSelectedValues(0);
     }
+
+    console.log('value', value);
+    console.log('selectedRadio', selectedRadio);
   };
 
   const getBackgroundColor = () => {
@@ -52,7 +63,7 @@ const TeacherPerformanceFeedBack = ({
   };
 
   // Mock data
-  const infodata = [
+  /* const infodata = [
     {
       info: 'Itsearviointi',
       disabled: true,
@@ -66,6 +77,35 @@ const TeacherPerformanceFeedBack = ({
       disabled: false,
     },
   ];
+ */
+
+  const infodata = evaluation.units.flatMap((unit) => {
+    return unit.assessments.flatMap((assessment) => [
+      {
+        info: 'Osaamistaito',
+        disabled: true,
+        units__id: unit._id,
+        answer: assessment.answer,
+      },
+      {
+        info: 'TPO:n havainto',
+        disabled: true,
+        units__id: unit._id,
+        answerSupervisor: assessment.answerSupervisor,
+      },
+      {
+        info: 'Opettajan merkintä',
+        disabled: false,
+        units__id: unit._id,
+        answerTeacher: assessment.answerTeacher,
+      }
+    ]);
+  });
+
+  const buttonData = infodata.filter(
+    (it) => it.units__id === unit._id
+  );
+ 
 
   return (
     <main
@@ -79,7 +119,7 @@ const TeacherPerformanceFeedBack = ({
         <p style={{ padding: '2px' }}>Osaa ohjatusti</p>
         <p style={{ padding: '4px' }}>Osaa itsenäisesti</p>
       </div>
-      <div>
+      {/* <div>
         {infodata.map((item, index) => (
           <div key={index} className='first-div-style'>
             <p style={{ width: '38%', marginTop: '10px' }}>{item.info}</p>
@@ -133,6 +173,61 @@ const TeacherPerformanceFeedBack = ({
                       />
                     }
                   />
+                </RadioGroup>
+              </FormControl>
+            </div>
+          </div>
+        ))}
+      </div> */}
+      <div>
+        {buttonData.map((item, index) => (
+          <div key={index} className='first-div-style'>
+            <p style={{ width: '38%', marginTop: '10px' }}>{item.info}</p>
+            <div style={{ marginTop: '10px' }}>
+              <FormControl>
+                <RadioGroup
+                  row
+                  aria-labelledby='demo-form-control-label-placement'
+                  name='position'
+                  //name={`position-${unit._id}`}
+                  onChange={(e) => handleRadioChange(e, unit, item.info)}
+                >
+                  <FormControlLabel
+                    value='Osaa ohjatusti'
+                    control={
+                      <Radio />
+                    }
+                    sx={{
+                      '& .MuiSvgIcon-root': {
+                        marginRight: '70px',
+                      },
+                    }}
+                    onChange={handleRadioChange}
+                    disabled={item.disabled}
+                    checked={
+                      item.answer === 1 ||
+                      item.answerSupervisor === 1 ||
+                      item.answerTeacher === 1
+                    }
+                  />
+                  <FormControlLabel
+                    value='Osaa itsenäisesti'
+                    control={
+                      <Radio />
+                    }
+                    sx={{
+                      '& .MuiSvgIcon-root': {
+                        marginRight: '8%',
+                      },
+                    }}
+                    onChange={handleRadioChange}
+                    disabled={item.disabled}
+                    checked={
+                      item.answer === 2 ||
+                      item.answerSupervisor === 2 ||
+                      item.answerTeacher === 2
+                    }
+                   />
                 </RadioGroup>
               </FormControl>
             </div>
