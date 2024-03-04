215c60e072daddeb311fc7ea6ecc00ec747e3b85
Closan Josephine <closjos@digitalentshelsinki.internal>
2024-03-04 15:52:18 +0200

    fix the code to match the figma design

--- client/src/components/NotificationBadge/NotificationBadge.jsx
+++ client/src/components/NotificationBadge/NotificationBadge.jsx
@@ -1,20 +1,19 @@
-import icone from '../../assets/Internallink.svg'
+import icone from '../../assets/Internallink.svg';
 import React, { useContext } from 'react';
 import AuthContext from '../../store/context/AuthContext';
 
 const NotificationBadge = (props) => {
-
-  const { user } = useContext(AuthContext)
+  const { user } = useContext(AuthContext);
   const role = user?.role;
-  const { number1, number2 } = props
-  let badgeStyle1 = {}
-  let badgeStyle2 = {}
+  const { number1, number2 } = props;
+  let badgeStyle1 = {};
+  let badgeStyle2 = {};
 
   if (role === 'teacher') {
     badgeStyle1 = {
       backgroundColor: '#FFC61E',
-      width: '60%',
-      height: '50%px',
+      width: '100%',
+      height: '50%',
     };
     badgeStyle2 = {
       backgroundColor: '#FFE49C',
@@ -24,8 +23,8 @@ const NotificationBadge = (props) => {
   } else if (role === 'supervisor') {
     badgeStyle1 = {
       backgroundColor: '#f5a3c7',
-      width: '180px',
-      height: '116px',
+      width: '100%',
+      height: '50%',
     };
     badgeStyle2 = {
       backgroundColor: '#ffdbeb',
@@ -35,53 +34,39 @@ const NotificationBadge = (props) => {
   } else if (role === 'customer') {
     badgeStyle1 = {
       backgroundColor: '#9FC9EB',
-      width: '180px',
-      height: '116px',
+      width: '100%',
+      height: '50%',
     };
     badgeStyle2 = {
       backgroundColor: '#D0E6F7',
       width: '180px',
       height: '116px',
     };
-
   }
 
   return (
     <div className='notificationbadge-container'>
       <div className='badge' style={badgeStyle1}>
-        <span className='numberstyle'>{number1}</span>
+        {/* <span className='numberstyle'>{number1}</span> */}
         <div className='bottom-container'>
-          <span className='text'>Uutta ilmoitusta</span>
-          <span className='icone-style'><img src={icone} alt="" /></span>
+          <div>
+            <span className='numberstyle'>{number1}</span>
+            <span className='text'>uutta ilmoitusta</span>
+          </div>
+          <span className='icone-style'>
+            <img src={icone} alt='' />
+          </span>
         </div>
       </div>
-      <div className='badge' style={badgeStyle2}>
+      {/* <div className='badge' style={badgeStyle2}>
         <span className='numberstyle'>{number2}</span>
         <div className='bottom-container'>
           <span className='text'>Uutta viestiä</span>
           <span className='icone-style'><img src={icone} alt="" /></span>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 };
 
 export default NotificationBadge;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
