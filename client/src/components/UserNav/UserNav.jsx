dced1c379bcb0e092396cacb5584f9e160698e8e
Tomoko-hub <haisaitomoco@gmail.com>
2024-03-04 15:05:53 +0200

    install mui/styles and mui/materials for humburger menu

--- client/src/components/UserNav/UserNav.jsx
+++ client/src/components/UserNav/UserNav.jsx
@@ -1,8 +1,13 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
-import { Icon } from '@iconify/react';
+
 import AuthContext from '../../store/context/AuthContext';
 import InternalApiContext from '../../store/context/InternalApiContext';
+
+import { Icon } from '@iconify/react';
+import { IconButton } from '@mui/material';
+import{ MenuIcon } from '@mui/icons-material/Menu';
+import { Drawer } from '@mui/material/Drawer';
 
 const UserNav = ({ checkUnsavedChanges, handleNavigation, destination }) => {
   const { user } = useContext(AuthContext);
@@ -10,8 +15,10 @@ const UserNav = ({ checkUnsavedChanges, handleNavigation, destination }) => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  // Clear chosen evaluation
-  useEffect(() => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  // Clear chosen evaluation : old version Navbar
+  /* useEffect(() => {
     const routesToExclude = [
       '/customer-list',
       '/unit-list',
@@ -29,16 +36,29 @@ const UserNav = ({ checkUnsavedChanges, handleNavigation, destination }) => {
       setEvaluation(null);
     }
   }, [location.pathname, evaluation]);
+ */
+
+  //hamburger
+  useEffect(() => {
+    const routesToExclude = ['/customer-list', '/unit-list', '/contract-info', '/userperformance'];
+    const isExcludedRoute = routesToExclude.some(route => location.pathname === route);
+    if (!isExcludedRoute && evaluation !== null && (user.role === 'teacher' || user.role === 'supervisor')) {
+      setEvaluation(null);
+    }
+  }, [location.pathname, evaluation, user.role, setEvaluation]);
+
 
   const handleIconClick = (destination) => {
     checkUnsavedChanges();
     handleNavigation(destination);
   };
 
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+
   return (
     <main className='userNav__wrapper'>
-      {/* Customer */}
-      {user && user.role === 'customer' && (
+      {/* Customer : old version Nav */}
+      {/* {user && user.role === 'customer' && (
         <div className={`userNav__icons ${user.role}`}>
           <Icon
             icon='ic:outline-home'
@@ -65,10 +85,10 @@ const UserNav = ({ checkUnsavedChanges, handleNavigation, destination }) => {
             }
           />{' '}
         </div>
-      )}
+      )} */}
 
-      {/* Supervisor */}
-      {user && user.role === 'supervisor' && (
+      {/* Supervisor : old Nav version */}
+      {/* {user && user.role === 'supervisor' && (
         <div className={`userNav__icons ${user.role}`}>
           <Icon
             icon='ic:outline-home'
@@ -97,10 +117,10 @@ const UserNav = ({ checkUnsavedChanges, handleNavigation, destination }) => {
             }
           />
         </div>
-      )}
+      )} */}
 
-      {/* Teacher */}
-      {user && user.role === 'teacher' && (
+      {/* Teacher : old Nav version */}
+      {/* {user && user.role === 'teacher' && (
         <div className={`userNav__icons ${user.role}`}>
           <Icon
             icon='ic:outline-home'
@@ -137,7 +157,35 @@ const UserNav = ({ checkUnsavedChanges, handleNavigation, destination }) => {
             }
           />
         </div>
-      )}
+      )} */}
+
+      {/* hamburger menu*/}
+      <div className="mobile-menu-button" onClick={toggleMenu}>
+        {/* Hamburger icon */}
+        <IconButton>
+          <MenuIcon />
+        </IconButton>
+      </div>
+      <div className={`userNav__menu ${isMenuOpen ? 'userNav__menu--open' : ''}`}>
+        <div className="userNav__menu__closeBtn" onClick={toggleMenu}>
+          x
+        </div>
+        {/* Menu Items */}
+        <div className={`userNav__icons ${user.role}`}>
+          {user.role !== 'customer' && evaluation && <Icon icon="bx:file" onClick={() => navigate('/contract-info')}/>}
+          {user.role === 'teacher' && <Icon icon="mingcute:group-line" onClick={() => navigate('/admin-menu')}/>} 
+          <div className='nav_menu_list'>
+            <div className='nav_menu_title'>
+              <h4>Profiili</h4>
+              <h4>Kirjaudu ulos</h4>
+              <Icon
+                icon="material-symbols:logout-rounded"
+                onClick={() => navigate('/profile')} 
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 };
