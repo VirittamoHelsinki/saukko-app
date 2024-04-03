20b4f3ab21995f39e9a86a61aef44e2e46473f2b
Joni Rinta-Kahila <rintjon@digitalentshelsinki.internal>
2024-04-03 15:43:43 +0300

    Implement new auth ctx provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 // importing all sass styling
 import './scss/index.scss';
 import axios from 'axios';
-import { AuthContextProvider } from './store/context/AuthContext';
+import { AuthContextProvider as OldAuthContextProvider } from './store/context/AuthContext';
 import { ExternalApiContextProvider } from './store/context/ExternalApiContext';
 import { InternalApiContextProvider } from "./store/context/InternalApiContext";
 
 // importing page routing
 import Router from "./components/Router/Router";
 import ErrorBoundary from './components/errorBoundary';
+import AuthContextProvider from './store/context/authContextProvider';
 
 axios.defaults.withCredentials = true;
 
@@ -16,13 +17,15 @@ const App = () => {
   return (
     <main className='app__wrapper'>
       <ErrorBoundary>
-        <AuthContextProvider>
-          <InternalApiContextProvider>
-            <ExternalApiContextProvider>
-              <Router />
-            </ExternalApiContextProvider>
-          </InternalApiContextProvider>
-        </AuthContextProvider>
+        <OldAuthContextProvider>
+          <AuthContextProvider>
+            <InternalApiContextProvider>
+              <ExternalApiContextProvider>
+                <Router />
+              </ExternalApiContextProvider>
+            </InternalApiContextProvider>
+          </AuthContextProvider>
+        </OldAuthContextProvider>
       </ErrorBoundary>
     </main>
   );
