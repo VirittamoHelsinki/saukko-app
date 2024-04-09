d4d694eb841785dae5686a00b9fddf5253c99561
Tomoko-hub <haisaitomoco@gmail.com>
2024-04-09 19:31:26 +0300

    fix box style: display pagelist for mobile, burger icon won't show when mobile menu is open

--- client/src/components/UserNav/UserNav.jsx
+++ client/src/components/UserNav/UserNav.jsx
@@ -47,10 +47,12 @@ const UserNav = ({ checkUnsavedChanges, handleNavigation }) => {
     <main className='userNav__wrapper'>
       {/* for hamburger menu*/}
       {/* teacher */}
-      <div className='mobile-menu-button' onClick={toggleMenu}>
-        {/* Hamburger icon */}
-        <Icon icon='ci:menu-alt-05' />
-      </div>
+      {!isMenuOpen && (
+        <div className='mobile-menu-button' onClick={toggleMenu}>
+          {/* Hamburger icon */}
+          <Icon icon='ci:menu-alt-05' />
+        </div>
+      )}
       <div
         className={`userNav__menu ${isMenuOpen ? 'userNav__menu--open' : ''}`}
       >
@@ -59,235 +61,130 @@ const UserNav = ({ checkUnsavedChanges, handleNavigation }) => {
         </div>
         {/* Hamburger  Menu Items */}
         {/* set background color user's role */}
-        <div 
-          sx={{bgcolor: 'text.disabled'}}
+        <div
+          sx={{ bgcolor: 'text.disabled' }}
           className={`userNav__icons ${user.role}`}>
-          <Box 
-            sx={{ height: '20vh', width: '18vw', textAlign: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+          <Box
+            sx={{ height: '20vh', width: '18vw', textAlign: 'center', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <img
-              sx={{width: '100%'}} 
+              sx={{ width: '100%' }}
               src={HelsinkiLogo} alt='' />
             <h1>OsTu</h1>
           </Box>
           {user && user.role === 'teacher' && (
-          <Box 
-            alignItems='center'
-            sx={{ 
-              marginTop: '-50px', width:'35%', height:'50%',
-              fontSize:'32px', fontWeight: '600', cursor: 'pointer'
-               }}
+            <Box
+              alignItems='left'
+              style={{ maxHeight: '100%', overflowY: 'auto', marginLeft: '-30px' }}
+              sx={{
+                marginTop: '12px',
+                width: '45%',
+                height: '100vh',
+                fontSize: '32px',
+                fontWeight: '600',
+                cursor: 'pointer',
+                //bgcolor: 'text.disabled',
+                display: 'flex',
+                flexDirection: 'column'
+              }}
             >
-            <div style={{ textAlign: 'left'}}>
-              <List>
-                <ListItem style={{ marginBottom: '20px',paddingBottom: '15px' }}>
-                  <Typography
+              <div style={{ textAlign: 'left', flexGrow: 1 }}>
+                <List>
+                  <ListItem style={{ marginBottom: '20px', paddingBottom: '10px' }}>
+                    <Typography
                       className='NavText'
                       onClick={() => {
-                       toggleMenu();
-                      navigate('/');
-                    }}
-                  >
-                    Etusivu
-                  </Typography>
-                </ListItem>
-                <ListItem style={{ marginBottom: '20px', paddingBottom: '15px' }}>
-                  <Typography
-                    className='NavText'
-                    onClick={() => {
-                      toggleMenu();
-                      // Check for unsaved changes and navigate if needed
-                      if (checkUnsavedChanges) {
-                        handleIconClick('/degrees/add');
-                      } else {
-                        navigate('/degrees/add');
+                        toggleMenu();
+                        navigate('/');
+                      }}
+                    >
+                      Etusivu
+                    </Typography>
+                  </ListItem>
+                  <ListItem style={{ marginBottom: '20px', paddingBottom: '10px' }}>
+                    <Typography
+                      className='NavText'
+                      onClick={() => {
+                        toggleMenu();
+                        // Check for unsaved changes and navigate if needed
+                        if (checkUnsavedChanges) {
+                          handleIconClick('/degrees/add');
+                        } else {
+                          navigate('/degrees/add');
+                        }
+                      }}
+                      style={{ height: '100%' }}
+                    >
+                      Tutkinnot
+                    </Typography>
+                  </ListItem>
+                  <ListItem style={{ marginBottom: '20px', paddingBottom: '10px' }}>
+                    <Typography
+                      onClick={() => {
+                        toggleMenu();
+                        navigate('/add/companyname');
                       }
-                    }}
-                    style={{ height: '100%' }}
-                    >
-                    Tutkinnot
-                  </Typography>
-                </ListItem>
-                <ListItem style={{ marginBottom: '20px',paddingBottom: '15px' }}>
-                  <Typography
-                    onClick={() => {
-                      toggleMenu();
-                      navigate('/add/companyname');
                       }
-                    }
-                    className='NavText'
+                      className='NavText'
                     >
-                    Työpaikat
-                  </Typography>
-                </ListItem>
-                <ListItem style={{ marginBottom: '20px', paddingBottom: '15px' }}>
-                  <Typography 
-                    onClick={()=>{
-                      toggleMenu();
-                      navigate('/evaluation-form');
-                  }}
-                    className='NavText'
+                      Työpaikat
+                    </Typography>
+                  </ListItem>
+                  <ListItem style={{ marginBottom: '20px', paddingBottom: '10px' }}>
+                    <Typography
+                      onClick={() => {
+                        toggleMenu();
+                        navigate('/evaluation-form');
+                      }}
+                      className='NavText'
                     >
-                    + Luo uusi sopimus
-                  </Typography>
-                </ListItem>
-                <ListItem style={{ marginBottom: '20px', paddingBottom: '15px' }}>
-                  <Typography
-                    onClick={() => {
-                      toggleMenu();
-                      navigate('/'); // ominaiisuus ei vielä valmis:sprint 12
-                    }}
-                    className='NavText'
+                      + Luo uusi sopimus
+                    </Typography>
+                  </ListItem>
+                  <ListItem style={{ marginBottom: '20px', paddingBottom: '10px' }}>
+                    <Typography
+                      onClick={() => {
+                        toggleMenu();
+                        navigate('/'); // ominaiisuus ei vielä valmis:sprint 12
+                      }}
+                      className='NavText'
                     >
-                    Asiakkuudet
-                  </Typography>
-                </ListItem>
-                <ListItem style={{ marginBottom: '20px', paddingBottom: '15px' }}>
-                  <Typography
-                    onClick={() => {
-                      toggleMenu();
-                      navigate('/'); // ominaiisuus ei vielä valmis:sprint 12
-                    }}
-                    className='NavText'
+                      Asiakkuudet
+                    </Typography>
+                  </ListItem>
+                  <ListItem style={{ marginBottom: '20px', paddingBottom: '10px' }}>
+                    <Typography
+                      onClick={() => {
+                        toggleMenu();
+                        navigate('/'); // ominaiisuus ei vielä valmis:sprint 12
+                      }}
+                      className='NavText'
                     >
                       Opettajat
-                  </Typography>
-                </ListItem>
-                <ListItem style={{ marginBottom: '20px', paddingBottom: '15px' }}>
-                  <Typography
-                    className='NavText'
-                    onClick={() =>{
-                      toggleMenu(); 
-                      navigate('/profile');}}
+                    </Typography>
+                  </ListItem>
+                  <ListItem style={{ marginBottom: '20px' }}>
+                    <Typography
+                      className='NavText'
+                      onClick={() => {
+                        toggleMenu();
+                        navigate('/profile');
+                      }}
                     >
                       Profiili
-                  </Typography>
-                </ListItem>
-              </List>
-              {/* tästä entinen koodi */}
-              {/* <Typography
-                className='NavText'
-                sx={{ fontWeight: '600', cursor: 'pointer', marginBottom: '10px', fontSize:'32px' }}
-                onClick={() => {
-                  toggleMenu();
-                  navigate('/');
-                }}
-              >
-                Home
-              </Typography>
-              <Typography
-                className='NavText'
-                sx={{ fontWeight: '600', cursor: 'pointer', marginBottom: '10px', fontSize:'32px' }}
-                onClick={() => {
-                  toggleMenu();
-                  // Check for unsaved changes and navigate if needed
-                  if (checkUnsavedChanges) {
-                    handleIconClick('/degrees/add');
-                  } else {
-                    navigate('/degrees/add');
-                  }
-                }}
-              >
-                Tutkinnot
-              </Typography>
-              <Typography
-                onClick={() => {
-                  toggleMenu();
-                  navigate('/add/companyname');
-                  }
-                }
-                className='NavText'
-                sx={{ fontWeight: '600', cursor: 'pointer', marginBottom: '10px', fontSize:'32px'  }}
-                >Työpaikat</Typography>
-              <Typography 
-                onClick={()=>{
-                  toggleMenu();
-                  navigate('/evaluation-form');
-              }}
-                className='NavText'
-                sx={{ fontWeight: '600', cursor:'pointer', marginBottom: '10px', fontSize:'32px' }}
-                >
-                + Luo uusi sopimus
-              </Typography>
-              <Typography
-                onClick={() => {
-                  toggleMenu();
-                  navigate('/'); // ominaiisuus ei vielä valmis:sprint 12
-                }}
-                className='NavText'
-                sx={{ fontWeight: '600', cursor: 'pointer', marginBottom: '10px', fontSize:'32px'  }}
-                >Asiakkuudet</Typography>
-              <Typography
-                onClick={() => {
-                  toggleMenu();
-                  navigate('/'); // ominaiisuus ei vielä valmis:sprint 12
-                }}
-                className='NavText'
-                sx={{ fontWeight: '600', cursor: 'pointer', marginBottom: '10px', fontSize:'32px'  }}
-                >
-                  Opettajat
-              </Typography>
-              <Typography
-                className='NavText'
-                sx={{ fontWeight: '600', cursor: 'pointer', marginBottom: '10px', fontSize:'32px' }}
-                onClick={() =>{
-                  toggleMenu(); 
-                  navigate('/profile');}}
-                >
-                  Profiili
-              </Typography> */}
-            </div>
-          </Box>
-          )}
-          
-          {/* For customer and TPO */}
-          {user && user.role !== 'teacher' && (
-          <Box
-            alignItems='center'
-            sx={{ 
-              marginTop: '20px', width:'35%', height:'50%',
-              fontSize:'32px', fontWeight: '600', cursor: 'pointer'
-              }}>
-              <div className='nav-MenuList'>
-                  <div style={{ textAlign: 'left'}}>
-                    <ListItem style={{ marginBottom: '20px', paddingBottom: '15px' }}>
-                      <Typography
-                        className='NavText'
-                        sx={{ fontWeight: '600', cursor: 'pointer', fontSize:'32px'  }}
-                        onClick={() => {
-                          toggleMenu();
-                          navigate('/');
-                        }}
-                      >
-                        Etusivu
-                      </Typography>
-                    </ListItem>
-                    <ListItem style={{ marginBottom: '20px', paddingBottom: '15px' }}>
-                      <Typography
-                        className='NavText'
-                        sx={{ fontWeight: '600', cursor: 'pointer', fontSize:'32px' }}
-                        onClick={() =>{
-                          toggleMenu(); 
-                          navigate('/profile');}}
-                        >
-                        Profiili
-                      </Typography>
-                    </ListItem>
-                  </div>
+                    </Typography>
+                  </ListItem>
+                </List>
               </div>
-          </Box>    
-          )}
-              {/*  {user.role !== 'customer' && evaluation && <Icon icon="bx:file" onClick={() => navigate('/contract-info')}/>} */}
-              {/*  {user.role === 'teacher' && <Icon icon="mingcute:group-line" onClick={() => navigate('/admin-menu')}/>}  */}
-              {/* Kirjaudu ulos logo */}
               <Grid container alignItems='flex-start' justifyContent='flex-end'>
                 <Grid item>
                   <Button
-                    onClick={() =>{
-                      toggleMenu(); 
-                      navigate('/profile')}}
+                    onClick={() => {
+                      toggleMenu();
+                      navigate('/profile')
+                    }}
                     sx={{ marginRight: '20px', marginBottom: '20px', cursor: 'pointer' }}
                   >
-                    <Typography 
+                    <Typography
                       sx={{ fontWeight: '600', fontSize: '14px', cursor: 'pointer' }}>
                       Kirjaudu ulos
                     </Typography>
@@ -299,9 +196,70 @@ const UserNav = ({ checkUnsavedChanges, handleNavigation }) => {
                   </Button>
                 </Grid>
               </Grid>
+            </Box>
+
+          )}
+
+          {/* For customer and TPO */}
+          {user && user.role !== 'teacher' && (
+            <Box
+              alignItems='center'
+              sx={{
+                marginTop: '20px', width: '35%', height: '50%',
+                fontSize: '32px', fontWeight: '600', cursor: 'pointer'
+              }}>
+              <div style={{ textAlign: 'left' }}>
+                <ListItem style={{ marginBottom: '20px', paddingBottom: '15px' }}>
+                  <Typography
+                    className='NavText'
+                    sx={{ fontWeight: '600', cursor: 'pointer', fontSize: '32px' }}
+                    onClick={() => {
+                      toggleMenu();
+                      navigate('/');
+                    }}
+                  >
+                    Etusivu
+                  </Typography>
+                </ListItem>
+                <ListItem style={{ marginBottom: '20px', paddingBottom: '15px' }}>
+                  <Typography
+                    className='NavText'
+                    sx={{ fontWeight: '600', cursor: 'pointer', fontSize: '32px' }}
+                    onClick={() => {
+                      toggleMenu();
+                      navigate('/profile');
+                    }}
+                  >
+                    Profiili
+                  </Typography>
+                </ListItem>
+              </div>
+              <Grid container alignItems='flex-start' justifyContent='flex-end'>
+                <Grid item>
+                  <Button
+                    onClick={() => {
+                      toggleMenu();
+                      navigate('/profile')
+                    }}
+                    sx={{ marginRight: '20px', marginBottom: '20px', cursor: 'pointer' }}
+                  >
+                    <Typography
+                      sx={{ fontWeight: '600', fontSize: '14px', cursor: 'pointer' }}>
+                      Kirjaudu ulos
+                    </Typography>
+                    <Icon
+                      icon='websymbol:logout'
+                      color='black'
+                      style={{ marginLeft: '10px' }}
+                    />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Box>
+          )}
         </div>
       </div>
-    </main>
+    </main >
   );
 };
 
