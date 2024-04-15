442a57f7780b7803cdf64b8c2f337721563a2030
Closan Josephine <closjos@digitalentshelsinki.internal>
2024-04-15 09:57:34 +0300

    replace mock data in contract information - WIP

--- client/src/pages/ContractInfo/ContractInfo.jsx
+++ client/src/pages/ContractInfo/ContractInfo.jsx
@@ -1,48 +1,173 @@
 // Import react packages & dependencies
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 // Import components
 import WavesHeader from '../../components/Header/WavesHeader';
 import InfoList from '../../components/InfoList/InfoList';
-import Hyperlink from '../../components/Hyperlink/Hyperlink';
 import UserNav from '../../components/UserNav/UserNav';
+import InternalApiContext from '../../store/context/InternalApiContext';
+import { fetchInternalDegreeById } from '../../api/degree';
+import { all } from 'axios';
 
 const ContractInfo = () => {
+  const { evaluation } = useContext(InternalApiContext);
+  const [degreeDetails, setDegreeDetails] = useState(null);
+
+  useEffect(() => {
+    const degree = async () => {
+      try {
+        const response = await fetchInternalDegreeById(evaluation?.degreeId);
+        // console.log(response);
+        setDegreeDetails(response);
+      } catch (error) {
+        console.error(error);
+        throw error; // Throw the error for the caller to handle
+      }
+    };
+
+    degree();
+  }, [evaluation]);
+
+  function formatDate(dateString) {
+    const startDate = new Date(dateString);
+    const day = startDate.getDate().toString().padStart(2, '0'); // Get day with leading zero if necessary
+    const month = (startDate.getMonth() + 1).toString().padStart(2, '0'); // Get month with leading zero if necessary (Note: January is 0)
+    const year = startDate.getFullYear();
+    return `${day}.${month}.${year}`;
+  }
+
+  const startDateString = evaluation?.startDate; // Get start date from evaluation object
+  const endDateString = evaluation?.endDate; // Get end date from evaluation object
+
+  let supervisorNames;
+  if (evaluation?.supervisorIds && evaluation.supervisorIds.length > 0) {
+    supervisorNames = ''; // Reset the default message
+    evaluation.supervisorIds.forEach((supervisor) => {
+      const firstName = supervisor.firstName || '';
+      const lastName = supervisor.lastName || '';
+      supervisorNames += `${firstName} ${lastName}, `;
+    });
+    supervisorNames = supervisorNames.slice(0, -2);
+  }
+
   const data = [
     {
-      title: 'Asiakkaan nimi',
-      content: 'Alex Tenerio',
+      title: 'Nimi',
+      content: `${evaluation?.customerId?.firstName} ${evaluation?.customerId?.lastName}`,
     },
     {
-      title: 'Koulutus',
-      content: 'Tieto- ja viestintätekniikan perustutkinto',
+      title: 'Sähköposti',
+      content: 's-posti',
+    },
+    {
+      title: 'Asiakkuuden aloituspäivä',
+      content: formatDate(startDateString),
+      // content: evaluation ? evaluation.startDate.format('DD.MM.YYYY') : '',
+    },
+    {
+      title: 'Asiakkuuden lopeutuspäivä',
+      content: formatDate(endDateString),
     },
     {
       title: 'Työpaikka',
-      content: 'Lorem ipsum OY',
+      content: evaluation?.workplaceId?.name,
     },
     {
-      title: 'Opettaja',
-      content: 'Sanna Virtanen',
+      title: 'Y-tunnus',
+      content: evaluation?.workplaceId?.businessId,
     },
     {
-      title: 'Ohjaaja',
-      content: 'Jonna Koskinen',
+      title: 'Yksikkö',
+      content: evaluation?.workplaceId?.departmentId?.name || '-',
     },
+    {
+      title: 'Työpaikkaohjaaja',
+      content: supervisorNames,
+    },
+    {
+      title: 'TPO:n sähköposti',
+      content: 's-posti',
+    },
+    {
+      title: 'Tutkinnon nimi',
+      content: degreeDetails && degreeDetails?.name.fi,
+    },
+    {
+      title: 'Määräyksen diaarinumero',
+      content: degreeDetails && degreeDetails?.diaryNumber,
+    },
+    {
+      title: 'Määräyksen päättöspäivämäärä',
+      content:
+        degreeDetails && degreeDetails.regulationDate
+          ? formatDate(degreeDetails.regulationDate)
+          : '-',
+    },
+    {
+      title: 'Voimaantulo',
+      content:
+        degreeDetails && degreeDetails.validFrom
+          ? formatDate(degreeDetails.validFrom)
+          : '-',
+    },
+    {
+      title: 'Voimassaolo päätyminen',
+      content:
+        degreeDetails && degreeDetails.expiry
+          ? formatDate(degreeDetails.expiry)
+          : '-',
+    },
+    {
+      title: 'Siirtymäajan päättymisaika',
+      content:
+        degreeDetails && degreeDetails.transitionEnds
+          ? formatDate(degreeDetails.transitionEnds)
+          : '-',
+    },
+    {
+      title: 'Työtehtävä',
+      content: evaluation?.workTasks,
+    },
+    {
+      title: 'Omat tavoitteet',
+      content: evaluation?.workGoals,
+    },
+    // {
+    //   title: 'Opettaja',
+    //   content: 'Sanna Virtanen',
+    // },
   ];
   return (
     <main className='contractInfo__wrapper'>
       <WavesHeader
-        title='Saukko'
-        secondTitle={'Alex Tenerio'}
-        disabled={false}
+        title='Sopimus'
+        secondTitle={
+          evaluation?.customerId?.firstName +
+          ' ' +
+          evaluation?.customerId?.lastName
+        }
+        disabled={true}
       />
       <div className='contractInfo__container'>
-        <section>
-          <InfoList title='tiedot' data={data} />
+        <section className='contractInfo__container--description'>
+          <InfoList data={data} />
         </section>
         <section className='contractInfo__container--description'>
-          <h3>Tutkinnon suorittaneen osaaminen</h3>
+          <ul>
+            {evaluation &&
+              evaluation.units.map((unit, index) => (
+                <li key={index}>
+                  <h4>{unit.name.fi}</h4>
+                  {unit &&
+                    unit.assessments.map((assessment, index) => (
+                      <div key={index}>
+                        <h5>{assessment.name.fi}</h5>
+                      </div>
+                    ))}
+                </li>
+              ))}
+          </ul>
+          {/* <h3>Tutkinnon suorittaneen osaaminen</h3>
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Minima
             adipisci delectus soluta velit ducimus qui nihil nam reprehenderit.
@@ -58,7 +183,7 @@ const ContractInfo = () => {
             linkSource={
               'https://eperusteet.opintopolku.fi/#/fi/ammatillinenperustutkinto/3397336/tiedot'
             }
-          />
+          /> */}
         </section>
       </div>
       <UserNav />
