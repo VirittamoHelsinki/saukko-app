325585c0f878978fc9104445c212e8757d7be566
Joel6677 <joel.heusala@gmail.com>
2024-04-24 09:08:24 +0300

    set gmail

--- server/src/mailer/configMailer.ts
+++ server/src/mailer/configMailer.ts
@@ -3,23 +3,23 @@ import nodemailer from 'nodemailer';
 // General email sending function
 export const sendEmail = async ({ to, subject, html }: any) => {
 // for testing
-  const transporter = nodemailer.createTransport({
+/*  const transporter = nodemailer.createTransport({
     host: 'smtp.ethereal.email',
     port: 587,
     auth: {
       user: 'jamie.sauer86@ethereal.email',
       pass: '2FgEanEy4g4DUZt9wV',
     },
-  });
-// for real mail
-  /*  const transporter = nodemailer.createTransport({
+  });*/
+// for real gmail
+    const transporter = nodemailer.createTransport({
       service: process.env.SERVICE,
       host: process.env.EMAIL_SERVICE_HOST,
       auth: {
         user: process.env.EMAIL_SERVICE_USER,
         pass: process.env.EMAIL_SERVICE_PASSWORD,
       },
-    });*/
+    });
 
   // Define the email options
   const mailOptions = {
