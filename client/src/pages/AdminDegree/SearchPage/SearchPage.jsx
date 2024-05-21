f1e40098d2fe57ce41bfad070cd8d8296237f0c4
Joni Rinta-Kahila <rintjon@digitalentshelsinki.internal>
2024-05-21 08:13:01 +0300

    Fix linters

--- client/src/pages/AdminDegree/SearchPage/SearchPage.jsx
+++ client/src/pages/AdminDegree/SearchPage/SearchPage.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useHeadingContext } from '../../../store/context/headingContectProvider';
