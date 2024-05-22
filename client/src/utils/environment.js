70c56368d6407f78141c7ebb82fbf1693720e504
Joni Rinta-Kahila <rintjon@digitalentshelsinki.internal>
2024-05-22 07:26:36 +0300

    Remove old comments

--- client/src/utils/environment.js
+++ client/src/utils/environment.js
@@ -22,13 +22,13 @@ const environments = {
   },
   staging: {
     showTestEnvironmentWarning: true,
-    appInsightsInstrumentationKey: import.meta.env.APP_INSIGHTS_INSTRUMENTATION_KEY, //'bd7deb24-7b39-4832-bc15-2e18b6697064', // TODO: use ENV
+    appInsightsInstrumentationKey: import.meta.env.APP_INSIGHTS_INSTRUMENTATION_KEY,
     environentName: 'STAGING',
     eperusteetDataUrl: import.meta.env.VITE_EPERUSTEET_DATA_URL,
   },
   production: {
     showTestEnvironmentWarning: false,
-    appInsightsInstrumentationKey: import.meta.env.APP_INSIGHTS_INSTRUMENTATION_KEY, //"e3bae532-eb64-494a-8fb9-7f3663ca3d73", // TODO: use env,
+    appInsightsInstrumentationKey: import.meta.env.APP_INSIGHTS_INSTRUMENTATION_KEY,
     environentName: 'PRODUCTION',
     eperusteetDataUrl: import.meta.env.VITE_EPERUSTEET_DATA_URL,
   }
