74b6d5cb799d4016ea3f1846770dbab748e176b3
Joni Rinta-Kahila <rintjon@digitalentshelsinki.internal>
2024-05-29 08:20:24 +0300

    Refactor the evaluations controller

---
+++ server/src/controllers/evaluationController/deleteById.ts
@@ -0,0 +1,20 @@
+import EvaluationModel from '../../models/evaluatuionModel';
+import { Response } from 'express';
+import { Request } from '../../types/requestType';
+import { responseWithError } from './helper';
+
+const deleteById = async (req: Request, res: Response) => {
+  try {
+    const evaluation = await EvaluationModel.findById(req.params.id);
+    if (!evaluation) {
+      return res.status(404).send({ message: 'Evaluation not found.' });
+    }
+
+    await EvaluationModel.findByIdAndDelete(req.params.id);
+    res.status(204).send();
+  } catch (error) {
+    responseWithError(res, 500, error, 'Failed to delete evaluation');
+  }
+};
+
+export default deleteById;
