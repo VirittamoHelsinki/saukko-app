43eff15332b296448aa3bf35cb06dfa2834b9810
Oliver K <Corned@users.noreply.github.com>
2024-07-29 15:52:50 +0300

    load actual data, fixed z-index issue

--- client/src/components/Stepper/Stepper.jsx
+++ client/src/components/Stepper/Stepper.jsx
@@ -25,94 +25,39 @@ import { Icon } from '@iconify/react';
 import PropTypes from 'prop-types';
 
 const Stepper = ({ activePage, totalPages, data }) => {
-  console.log(activePage, totalPages, data);
-
-  const createSteppers = () => {
-    const steppers = [];
-
-    for (let page = 1; page <= totalPages; page++) {
-      const isActive = activePage === page;
-      const isDone = activePage > page;
-
-      steppers.push(
-        <div
-          key={page}
-          className={`circle ${isActive ? 'active' : isDone ? 'done' : ''}`}
-        >
-          {isDone ? (
-            <Icon icon="zondicons:checkmark" color="white"/>
-          ) : (
-            <span className={`number ${isActive ? 'active' : ''}`}>{page}</span>
-          )}
-        </div>
-      );
-
-      if (page !== totalPages) {
-        steppers.push(
-          <div
-            key={`line-${page}`}
-            className={`line ${activePage >= page + 1 ? 'active' : ''}`}
-          />
-        );
-      }
-    }
-
-    return steppers;
-  };
-
-
-  const labels = [ "Tutkintotiedot", "Valitse tutkinnonosat", "Määritä tehtävät", "Yhteenveto" ]
-
-
-
   return (
+    <div className="steps" style={{ gridTemplateColumns: `repeat(${totalPages}, 1fr)` }}>
 
-    <div clasName="new-stepper">
-      <div className="steps" style={{ gridTemplateColumns: 'repeat(4, 1fr)'}}>
+      {
+        data.map(({label}, page) => {
+          page = page + 1
+          const isActive = activePage === page;
+          const isDone = activePage > page;
 
-        <div className="step-container">
-          <div className="circle done">
-            <Icon icon="zondicons:checkmark" color="white"/>
-            {/* <span className="number">{ 1 }</span> */}
-          </div>
-          <span>Tutkintotiedot</span>
-        </div>
+          const circleClassName = [
+            "circle",
+            (isActive ? "active" : ""),
+            (isDone ? "done" : ""),
+          ].join(" ")
 
-        <div className="step-container">
-          <div className="circle active">
-{/*             <Icon icon="zondicons:checkmark" color="white"/> */}
-            <span className="number active">{ 2 }</span>
-          </div>
-          <span>Valitse tutkinnonosat</span>
-        </div>
+          console.log(page, isActive, isDone, circleClassName);
 
-        <div className="step-container">
-          <div className="circle">
-{/*             <Icon icon="zondicons:checkmark" color="white"/> */}
-            <span className="number">{ 3 }</span>
-          </div>
-          <span>Määritä tehtävät</span>
-        </div>
-        
-        <div className="step-container">
-          <div className="circle">
-{/*             <Icon icon="zondicons:checkmark" color="white"/> */}
-            <span className="number">{ 4 }</span>
-          </div>
-          <span>Yhteenveto</span>
-        </div>
-
-
-      </div>
-
- {/*      <div className="text" style={{ gridTemplateColumns: `0.5fr auto 1fr auto 1fr auto 1fr auto 0.5fr` }}>
-        {
-          labels.map((text, i) => <span style={{ gridColumn: `${i * 2 + 2}` }}>{ "hi" }</span>)
-        }
-      </div> */}
+          return (
+            <div className="step-container">
+              <div className={`circle ${ circleClassName }`}>
+                {
+                  isDone
+                  ? <Icon icon="zondicons:checkmark" color="white"/>
+                  : <span className={`number ${isActive ? 'active' : ''}`}>{page}</span>
+                }
+              </div>
+              <span lang="fi">{ label }</span>
+            </div>
+          )
+        })
+      }
 
     </div>
-
   );
 };
 
