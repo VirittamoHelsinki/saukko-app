40611849bb39bfe9f2c7bd4d4d972aec499d7660
corned <oliver.kaarnalehto@gmail.com>
2024-08-14 13:50:32 +0300

    notification page

---
+++ client/src/pages/Home/Notification/Notification.jsx
@@ -0,0 +1,21 @@
+import { useEffect } from 'react';
+
+import { useHeadingContext } from '../../../store/context/headingContectProvider';
+
+const Notification = () => {
+  const { setSiteTitle, setSubHeading, setHeading } = useHeadingContext();
+
+  useEffect(() => {
+    setHeading('Ilmoitukset');
+    setSiteTitle('Ilmoitukset');
+    setSubHeading('Ilmoitukset');
+  }, [ ]);
+
+  return (
+    <div className='notification__wrapper'>
+      <p>Ilmoitukset :P</p>
+    </div>
+  );
+};
+
+export default Notification;
