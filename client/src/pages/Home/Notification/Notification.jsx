a465ca108064a4cac6251a42fb24973153046c56
corned <oliver.kaarnalehto@gmail.com>
2024-08-14 16:20:25 +0300

    notification page has two filter select inputs

--- client/src/pages/Home/Notification/Notification.jsx
+++ client/src/pages/Home/Notification/Notification.jsx
@@ -1,9 +1,19 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 import { useHeadingContext } from '../../../store/context/headingContectProvider';
 
+import Select from "@mui/material/select"
+import { MenuItem } from '@mui/material';
+import FormControl from "@mui/material/FormControl"
+
+
 const Notification = () => {
   const { setSiteTitle, setSubHeading, setHeading } = useHeadingContext();
+  const [ value, setValue ] = useState("a")
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  }
 
   useEffect(() => {
     setHeading('Ilmoitukset');
@@ -16,8 +26,35 @@ const Notification = () => {
   ]);
 
   return (
-    <div className='notification__wrapper'>
-      <p>Ilmoitukset :P</p>
+    <div className='notifications__wrapper'>
+      <h3>Suodatin</h3>
+
+      <div className="notifications-filter">
+        <Select
+          sx={{ borderRadius: 0, height: "40px" }}
+          value={value}
+          displayEmpty
+          onChange={handleChange}
+          placeholder='Valitse asiakas'
+          >
+          <MenuItem value={"a"}>Osteri Ossi</MenuItem>
+          <MenuItem value={"c"}>Nakke Nakuttaja</MenuItem>
+          <MenuItem value={"d"}>Nalle Puh</MenuItem>
+        </Select>
+        <Select
+          sx={{ borderRadius: 0, height: "40px" }}
+          value={value}
+          displayEmpty
+          onChange={handleChange}
+          placeholder='Valitse asiakas'
+          >
+          <MenuItem value={"a"}>Osteri Ossi</MenuItem>
+          <MenuItem value={"c"}>Nakke Nakuttaja</MenuItem>
+          <MenuItem value={"d"}>Nalle Puh</MenuItem>
+        </Select>
+
+      </div>
+
     </div>
   );
 };
