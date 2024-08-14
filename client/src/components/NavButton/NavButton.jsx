d61f893b35fba2a94fdef0ffe405e68d0dc6a29b
corned <oliver.kaarnalehto@gmail.com>
2024-08-14 14:31:48 +0300

    NavButton is now more flexible

--- client/src/components/NavButton/NavButton.jsx
+++ client/src/components/NavButton/NavButton.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 const NavButton = ({ children, style = {}, ...rest }) => {
   return (
-    <Link {...rest} style={{ color: inherit, textDecoration: "none", cursor: "pointer", ...style }}>
+    <Link {...rest} style={{ color: 'inherit', textDecoration: "none", cursor: "pointer", ...style }}>
       { children }
     </Link>
   )
