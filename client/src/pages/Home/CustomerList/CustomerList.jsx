92143198f3c5b993a89eed65208290f93c84908d
Joel6677 <joel.heusala@gmail.com>
2024-08-21 10:32:04 +0300

    fixing linting errors and evaluations seems to work with all roles

--- client/src/pages/Home/CustomerList/CustomerList.jsx
+++ client/src/pages/Home/CustomerList/CustomerList.jsx
@@ -51,7 +51,7 @@ export default function CustomerList() {
 
   useEffect(() => {
     resetEvaluation();
-  }, [])
+  }, [resetEvaluation])
 
   const [isInfoButtonOpen, setIsInfoButtonOpen] = useState(false);
 
