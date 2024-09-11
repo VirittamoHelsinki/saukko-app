7e5c61944e7d272c4585041142a20b83b8d71b2a
corned <oliver.kaarnalehto@gmail.com>
2024-09-11 10:20:11 +0300

    all fields

--- client/src/pages/ClientPage/ClientPage.jsx
+++ client/src/pages/ClientPage/ClientPage.jsx
@@ -5,6 +5,20 @@ import { useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 
 
+const ClientInformationField = ({
+  fieldLabel = "field",
+  fieldValue = "-",
+  disabled = false,
+}) => {
+  return (
+    <div className={`client-information__field ${disabled ? "disabled" : ""}`}>
+      <p className="field">{ fieldLabel }</p>
+      <p className="value">{ fieldValue }</p>
+    </div>
+  )
+}
+
+
 function ClientPage() {
   const { setSiteTitle, setSubHeading, setHeading } = useHeadingStore();
 
@@ -16,17 +30,67 @@ function ClientPage() {
   });
 
   useEffect(() => {
-    setSiteTitle("Asiakas sivu")
+    setSiteTitle("Asiakkuuden yhteenveto")
     setSubHeading("")
-    setHeading("Asiakas sivu")
+    setHeading("Asiakkuuden yhteenveto")
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [ ]);
+
+  console.log(user);
+  
 
 
   return (
     <div className='clientPage__wrapper' id='main-wrapper'>
       <section className='clientPage__container'>
-        <p>{ JSON.stringify(user) }</p>
+
+        <p className="title">Asiakkuuden yhteenveto</p>
+
+        <div className="clientPage__client-information-container">
+          {/* <p>{ JSON.stringify(user) }</p> */}
+
+          <ClientInformationField
+            fieldLabel={"Nimi"}
+            fieldValue={`${user?.firstName} ${user?.lastName}`}
+          />
+          <ClientInformationField
+            fieldLabel="Sähköposti"
+            fieldValue={user?.email}
+          />
+          <ClientInformationField
+            fieldLabel="Asiakkuuden aloituspäivä"
+            fieldValue=""
+          />
+          <ClientInformationField
+            fieldLabel="Asiakkuuden lopetuspäivä"
+            fieldValue=""
+          />
+          <ClientInformationField
+            fieldLabel="Työpaikka"
+            fieldValue=""
+          />
+          <ClientInformationField
+            fieldLabel="Y-tunnus"
+            fieldValue=""
+          />
+          <ClientInformationField
+            fieldLabel="Työpaikan yksikkö"
+            fieldValue=""
+          />
+          <ClientInformationField
+            fieldLabel="Ohjaaja"
+            fieldValue=""
+          />
+          <ClientInformationField
+            fieldLabel="Opettaja"
+            fieldValue=""
+          />
+          <ClientInformationField
+            fieldLabel="Täydennysjakson päättymispäivä *"
+            disabled
+          />
+
+        </div>
 
       </section>
     </div>
