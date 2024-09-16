3e5773317a516357b279e9bb09e64ef87cc63ee0
corned <oliver.kaarnalehto@gmail.com>
2024-09-16 09:26:44 +0300

    getallusers with filter

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -4,8 +4,12 @@ import userModel from '../models/userModel';
 
 const getAllUsers = async (req: Request, res: Response) => {
   try {
-      const users = await userModel.find();
+      const query = req.query;
+      const users = await userModel.find(query);
+      console.log("users", users)
+    
       res.status(200).json(users);
+
   } catch (error) {
       res.status(500).json({ message: 'Error fetching users', error });
   }
